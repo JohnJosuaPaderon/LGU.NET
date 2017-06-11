a8295d05e7a37c2e913adf46e52dc4d3de52c852
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-11 19:34:13 +0800

    Removed: Bindable.cs Updated: LGU.NetStandard.csproj Updated: LGUException.Definitions.Instantiation.cs

--- LGU.NetStandard/LGUException.Definitions.Instantiation.cs
+++ LGU.NetStandard/LGUException.Definitions.Instantiation.cs
@@ -4,9 +4,19 @@ namespace LGU
 {
     partial class LGUException
     {
-        public static LGUException ArgumentNullException(string parameterName, string lguExceptionMessage)
+        public static LGUException ArgumentNull(string parameterName, string lguExceptionMessage)
         {
-            return new LGUException(lguExceptionMessage, new ArgumentNullException(parameterName));
+            return new LGUException(lguExceptionMessage, new ArgumentNullException(parameterName), LGUExceptionLevel.High);
+        }
+
+        public static LGUException NullReference(string lguExceptionMessage)
+        {
+            return new LGUException(lguExceptionMessage, new NullReferenceException(), LGUExceptionLevel.Fatal);
+        }
+
+        public static LGUException ArgumentNullOrWhiteSpace(string parameterName, string lguExceptionMessage)
+        {
+            return new LGUException(lguExceptionMessage, new ArgumentException("Value cannot be null or white space."), LGUExceptionLevel.High);
         }
     }
 }
