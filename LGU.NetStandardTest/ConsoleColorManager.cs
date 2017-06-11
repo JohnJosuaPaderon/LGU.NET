74480904e76193ba9b4a0e54f05ee7ab0042fc94
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-11 19:37:30 +0800

    Removed: App.xaml Removed: App.xaml.cs Added: ConsoleColorManager.cs Updated: LGU.NetStandardTest.csproj Removed: logo.png Removed: MainWindow.xaml Removed: MainWindow.xaml.cs Added: Program.cs Added: TestModule.cs

---
+++ LGU.NetStandardTest/ConsoleColorManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LGU.NetStandardTest
+{
+    public class ConsoleColorManager
+    {
+        public static void InputHeader()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+
+        public static void Input()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+
+        public static void Error()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+
+        public static void Default()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
