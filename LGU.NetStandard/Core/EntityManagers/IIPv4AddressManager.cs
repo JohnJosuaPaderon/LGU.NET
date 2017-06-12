04d6e9c8a1c2a4c22484954092d4d065f014e101
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-12 12:14:34 +0800

    Added: IIPv4AddressManager.cs, IPv4AddressManager.cs

---
+++ LGU.NetStandard/Core/EntityManagers/IIPv4AddressManager.cs
@@ -0,0 +1,12 @@
+﻿using LGU.Core.Entities;
+
+namespace LGU.Core.EntityManagers
+{
+    public interface IIPv4AddressManager
+    {
+        IPv4Address ConvertFromString(string value);
+        IPv4Address ConvertFromByteArray(byte[] value);
+        string ConvertToString(IPv4Address ipv4Address);
+        byte[] ConvertToByteArray(IPv4Address ipv4Address);
+    }
+}
