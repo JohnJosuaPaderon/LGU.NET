84901dc888beaea3e9153793e2f8c693fa0069f8
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-13 21:40:46 +0800

    Added: MainWindow.xaml; MainWindow.xaml.cs; UserLoginView.xaml; UserLoginView.xaml.cs

---
+++ LGU.Desktop/Core/Views/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using LGU.Core.ViewModels;
+using MahApps.Metro.Controls;
+
+namespace LGU.Core.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += (s, e) => ViewModel.Initialize();
+        }
+
+        public MainWindowViewModel ViewModel => DataContext as MainWindowViewModel;
+    }
+}
