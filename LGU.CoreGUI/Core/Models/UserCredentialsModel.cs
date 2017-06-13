59a89ce6700a43a405a0d9acc5d1c15020a98590
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-13 14:03:35 +0800

    Added: UserCredentialsModel.cs

---
+++ LGU.CoreGUI/Core/Models/UserCredentialsModel.cs
@@ -0,0 +1,21 @@
+﻿using Prism.Mvvm;
+
+namespace LGU.Core.Models
+{
+    public class UserCredentialsModel : BindableBase
+    {
+        private string _Username;
+        public string Username
+        {
+            get { return _Username; }
+            set { SetProperty(ref _Username, value); }
+        }
+
+        private string _Password;
+        public string Password
+        {
+            get { return _Password; }
+            set { SetProperty(ref _Password, value); }
+        }
+    }
+}
