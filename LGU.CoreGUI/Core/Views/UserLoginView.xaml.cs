0b8bcb66de7abe50cafdcd3c5bdf70764c273de1
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-13 14:04:11 +0800

    Added: UserLoginView.xaml, UserLoginView.xaml.cs

---
+++ LGU.CoreGUI/Core/Views/UserLoginView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Core.Views
+{
+    /// <summary>
+    /// Interaction logic for UserLoginView
+    /// </summary>
+    public partial class UserLoginView : UserControl
+    {
+        public UserLoginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
