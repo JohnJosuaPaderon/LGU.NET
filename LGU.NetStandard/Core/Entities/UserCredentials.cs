c3800c98ca733267e9f6810565e89eb72be1da7a
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-13 14:06:54 +0800

    Updated: UserCredentials.cs

--- LGU.NetStandard/Core/Entities/UserCredentials.cs
+++ LGU.NetStandard/Core/Entities/UserCredentials.cs
@@ -1,8 +1,10 @@
-﻿namespace LGU.Core.Entities
+﻿using System.Security;
+
+namespace LGU.Core.Entities
 {
     public struct UserCredentials
     {
-        public string SecureUsername { get; }
-        public string SecurePassword { get; }
+        public SecureString SecureUsername { get; set; }
+        public SecureString SecurePassword { get; set; }
     }
 }
