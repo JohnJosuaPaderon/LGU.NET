d1d2fe94906340b6f4f57cbf6edfdbc595a6dff0
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-16 00:08:14 +0800

    Added: ContentSelectionView.xaml; ContentSelectionView.xaml.cs; HumanResourceLauncherView.xaml; HumanResourceLauncherView.xaml.cs

---
+++ LGU.Desktop/HumanResource/Views/HumanResourceLauncherView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.HumanResource.Views
+{
+    /// <summary>
+    /// Interaction logic for HumanResourceLauncherView
+    /// </summary>
+    public partial class HumanResourceLauncherView : UserControl
+    {
+        public HumanResourceLauncherView()
+        {
+            InitializeComponent();
+        }
+    }
+}
