6b7bcad457fce2f5a5ad70035f41c5ed0c388c23
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-16 00:06:58 +0800

    Added: UserAuthenticationView.xaml; UserAuthenticationView.xaml.cs; UserSignUpView.xaml; UserSignUpView.xaml.cs
    Updated: MainWindow.xaml; UserLoginView.xaml

---
+++ LGU.Desktop/Core/Views/UserAuthenticationView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Core.Views
+{
+    /// <summary>
+    /// Interaction logic for UserAuthenticationView
+    /// </summary>
+    public partial class UserAuthenticationView : UserControl
+    {
+        public UserAuthenticationView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LGU.Desktop/Core/Views/UserSignUpView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Core.Views
+{
+    /// <summary>
+    /// Interaction logic for UserSignUpView
+    /// </summary>
+    public partial class UserSignUpView : UserControl
+    {
+        public UserSignUpView()
+        {
+            InitializeComponent();
+        }
+    }
+}
