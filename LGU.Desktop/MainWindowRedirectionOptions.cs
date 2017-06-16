03712fdea2806f4aec7aae1527bb44ead7541428
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-17 05:56:11 +0800

    Added: MainWindowRedirectionOptions.cs Updated: LGU.Desktop.csproj; packages.config

---
+++ LGU.Desktop/MainWindowRedirectionOptions.cs
@@ -0,0 +1,26 @@
+﻿namespace LGU
+{
+    public class MainWindowRedirectionOptions
+    {
+        public static void Redirect(string target)
+        {
+            if (string.IsNullOrWhiteSpace(target))
+            {
+                throw LGUException.ArgumentNullOrWhiteSpace(nameof(target), "Redirection target is null or white space.");
+            }
+
+            Instance = new MainWindowRedirectionOptions(true, target);
+        }
+
+        public static MainWindowRedirectionOptions Instance { get; private set; }
+
+        private MainWindowRedirectionOptions(bool redirectToTarget, string target)
+        {
+            RedirectToTarget = redirectToTarget;
+            Target = target;
+        }
+
+        public bool RedirectToTarget { get; }
+        public string Target { get; }
+    }
+}
