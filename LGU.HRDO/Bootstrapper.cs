f8f0a5584059f73f8fda0340c415521a209c583a
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-18 19:31:05 +0800

    Added: Bootstrapper.cs Updated: App.xaml; LGU.HRDO.csproj Removed: MainWindow.xaml; MainWindow.xaml.cs

---
+++ LGU.HRDO/Bootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LGU.HRDO
+{
+    class Bootstrapper : UnityBootstrapper 
+    {
+    }
+}
