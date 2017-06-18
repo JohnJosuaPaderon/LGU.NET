94e5bdebc02355058053a77f1dbad90c6cca1007
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-19 00:24:27 +0800

    Added: Bootstrapper.cs; logo.ico Updated: App.config; App.xaml; LGU.HRDO.TimeLog.csproj

--- LGU.HRDO.TimeLog/App.xaml.cs
+++ LGU.HRDO.TimeLog/App.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 namespace LGU.HRDO.TimeLog
 {
@@ -13,5 +7,12 @@ namespace LGU.HRDO.TimeLog
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
     }
 }
---
+++ LGU.HRDO.TimeLog/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using LGU.Core.ViewModels;
+using LGU.Core.Views;
+using LGU.HumanResource.Views;
+using Prism.Unity;
+using System.Windows;
+
+namespace LGU.HRDO.TimeLog
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterTypeForNavigation<TimeLogView>();
+            MainWindowViewModel.ContentRegionMainTarget = nameof(TimeLogView);
+        }
+    }
+}
