9779131dfb73fd09a56ed59dc5aff9449b99f4e2
johnpad53 <johnjosua.paderon@outlook.com>
2017-06-23 16:54:52 +0800

    Added: InsertComputer.cs

---
+++ LGU.NetFramework/Core/EntityProcesses/InsertComputer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LGU.Core.Entities;
+
+namespace LGU.Core.EntityProcesses
+{
+    public class InsertComputer : IInsertComputer
+    {
+        public Computer Computer { get; private set; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Computer Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
