783c154c069a95a6776086187c6c48630085323d
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-04 06:12:32 +0800

    Added: Bootstrapper.cs; Updated: App.xaml; App.xaml.cs Removed: MainWindow.xaml; MainWindow.xaml.cs

--- LGU.HumanResource/App.xaml.cs
+++ LGU.HumanResource/App.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using LGU.ViewModels;
+using LGU.Views.HumanResource;
 using System.Windows;
 
 namespace LGU.HumanResource
@@ -13,5 +9,12 @@ namespace LGU.HumanResource
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindowViewModel.InitialMainContentRegionSource = nameof(TimeKeepingView);
+            new Bootstrapper().Run();
+        }
     }
 }
---
+++ LGU.HumanResource/Bootstrapper.cs
@@ -0,0 +1,15 @@
+﻿using LGU.Views.HumanResource;
+using Prism.Unity;
+
+namespace LGU.HumanResource
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+            Container.RegisterTypeForNavigation<TimeKeepingView>();
+            Container.RegisterTypeForNavigation<SampleView>();
+        }
+    }
+}
