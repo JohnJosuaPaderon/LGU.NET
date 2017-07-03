eef00a096877c96477db97b12c83f75818e2a586
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-04 06:17:39 +0800

    Added: BootstrapperBase.cs; logo.ico; Updated: LGU.WindowsUI.csproj

---
+++ LGU.WindowsUI/BootstrapperBase.cs
@@ -0,0 +1,19 @@
+﻿using LGU.Views;
+using Prism.Unity;
+using System.Windows;
+
+namespace LGU
+{
+    public abstract class BootstrapperBase : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.TryResolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
