b3e438690c752a45830be5a099fe591da2697dde
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-04 06:14:55 +0800

    Added: TimeKeepingViewModel.cs

---
+++ LGU.WindowsUI/ViewModels/HumanResource/TimeKeepingViewModel.cs
@@ -0,0 +1,26 @@
+﻿using LGU.Events;
+using LGU.Views.HumanResource;
+using Prism.Events;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace LGU.ViewModels.HumanResource
+{
+    public class TimeKeepingViewModel : BindableBase
+    {
+        private readonly IRegionManager RegionManager;
+        private readonly IEventAggregator EventAggregator;
+
+        public TimeKeepingViewModel(IRegionManager regionManager, IEventAggregator eventAggregator)
+        {
+            RegionManager = regionManager;
+            EventAggregator = eventAggregator;
+        }
+
+        public void Load()
+        {
+            EventAggregator.GetEvent<TitleEvent>().Publish("Time-Keeping");
+            RegionManager.RequestNavigate("SampleRegion", nameof(SampleView));
+        }
+    }
+}
