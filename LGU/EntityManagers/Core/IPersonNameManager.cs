6ee9ac7fda65c9eeff2e7e163d857a8a23fd1aee
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-03 18:02:26 +0800

    Added: IPersonNameManager.cs; IUserManager.cs; PersonNameManager.cs; UserManager.cs

---
+++ LGU/EntityManagers/Core/IPersonNameManager.cs
@@ -0,0 +1,11 @@
+﻿using LGU.Entities.Core;
+
+namespace LGU.EntityManagers.Core
+{
+    public interface IPersonNameManager
+    {
+        IDataProcessResult<string> ConstructFullName(Person person);
+        IDataProcessResult<string> ConstructMiddleInitials(Person person);
+        IDataProcessResult<string> ConstructInformalFullName(Person person);
+    }
+}
---
+++ LGU/EntityManagers/Core/IUserManager.cs
@@ -0,0 +1,19 @@
+﻿using LGU.Entities;
+using LGU.Entities.Core;
+
+namespace LGU.EntityManagers.Core
+{
+    public interface IUserManager : IEntityManager<User, ulong>
+    {
+    }
+
+    public interface IAsyncUserManager : IAsyncEntityManager<User, ulong>
+    {
+
+    }
+
+    public interface ICancellableAsyncUserManager : ICancellableAsyncEntityManager<User, ulong>
+    {
+
+    }
+}
