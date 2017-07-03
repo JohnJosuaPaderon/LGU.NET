d7fd109f09b27f3b720fd9888bdc4b58ae108f13
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-04 06:16:30 +0800

    Added: TimeKeepingView.xaml; TImeKeepingView.xaml.cs

---
+++ LGU.WindowsUI/Views/HumanResource/TimeKeepingView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using LGU.ViewModels.HumanResource;
+using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource
+{
+    /// <summary>
+    /// Interaction logic for TimeKeepingView
+    /// </summary>
+    public partial class TimeKeepingView : UserControl
+    {
+        public TimeKeepingView()
+        {
+            InitializeComponent();
+            Loaded += (s, e) => ViewModel.Load();
+        }
+
+        public TimeKeepingViewModel ViewModel => DataContext as TimeKeepingViewModel;
+    }
+}
