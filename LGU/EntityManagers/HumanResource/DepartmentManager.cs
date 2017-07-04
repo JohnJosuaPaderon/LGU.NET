0d867e668f4da0e3c7f9f12a4844d0875d6b1274
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-04 09:21:58 +0800

    Added: DepartmentManager.cs; IDepartmentManager.cs

---
+++ LGU/EntityManagers/HumanResource/DepartmentManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using LGU.Entities.HumanResource;
+using LGU.EntityProcesses.HumanResource;
+
+namespace LGU.EntityManagers.HumanResource
+{
+    public sealed class DepartmentManager : IDepartmentManager
+    {
+        public DepartmentManager(IDeleteDepartment deleteDepartment, IGetDepartmentById getDepartmentById, IGetDepartmentList getDepartmentList, IInsertDepartment insertDepartment, IUpdateDepartment updateDepartment)
+        {
+
+        }
+
+        public IDataProcessResult<Department> Delete(Department data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataProcessResult<Department>> DeleteAsync(Department data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataProcessResult<Department>> DeleteAsync(Department data, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataProcessResult<Department> GetById(uint id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataProcessResult<Department>> GetByIdAsync(uint id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataProcessResult<Department>> GetByIdAsync(uint id, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerableDataProcessResult<Department> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerableDataProcessResult<Department>> GetListAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerableDataProcessResult<Department>> GetListAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataProcessResult<Department> Insert(Department data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataProcessResult<Department>> InsertAsync(Department data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataProcessResult<Department>> InsertAsync(Department data, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDataProcessResult<Department> Update(Department data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataProcessResult<Department>> UpdateAsync(Department data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataProcessResult<Department>> UpdateAsync(Department data, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
