64baecd117aa3d431b59286e5167e428c604a5cc
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-04 09:20:33 +0800

    Added: IAsyncDbHelper.cs; ICancellableAsyncDbHelper.cs; IDbConnectionEstablisher.cs; IDbHelper.cs; IDbQueryInfo.cs; SqlConnectionEstablisher.cs; SqlHelper.Definitions.IAsyncDbHelper.cs; SqlHelper.Definitions.ICancellableDbHelper.cs; SqlHelper.Definitions.IDbHelper.cs; SqlQueryInfo.cs

---
+++ LGU/Data/RDBMS/IDbHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Common;
+
+namespace LGU.Data.RDBMS
+{
+    public interface IDbHelper<TConnection, TTransaction, TCommand, TParameter, TDataReader>
+        where TConnection : DbConnection
+        where TTransaction : DbTransaction
+        where TCommand : DbCommand
+        where TParameter : DbParameter
+        where TDataReader : DbDataReader
+    {
+        IProcessResult ExecuteNonQuery(IDbQueryInfo<TConnection, TTransaction, TCommand, TParameter> queryInfo);
+        IDataProcessResult<T> ExecuteNonQuery<T>(IDataDbQueryInfo<T, TConnection, TTransaction, TCommand, TParameter> queryInfo);
+        IDataProcessResult<T> ExecuteReader<T>(IDbQueryInfo<TConnection, TTransaction, TCommand, TParameter> queryInfo, Func<TDataReader, IDataProcessResult<T>> getFromReader);
+        IEnumerableDataProcessResult<T> ExecuteReaderEnumerable<T>(IDbQueryInfo<TConnection, TTransaction, TCommand, TParameter> queryInfo, Func<TDataReader, IEnumerableDataProcessResult<T>> getFromReader);
+        IDataProcessResult<T> ExecuteScalar<T>(IDbQueryInfo<TConnection, TTransaction, TCommand, TParameter> queryInfo, Func<object, IDataProcessResult<T>> converter);
+    }
+}
