8a32588a03c5cd06b26ec529271c73c15f1e5759
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-06 06:21:42 +0800

    Added: EmployeeFingerPrintEnrollmentView.xaml; EmployeeFingerPrintEnrollmentView.xaml.cs; PreviewEmployeeView.xaml; PreviewEmployeeView.xaml.cs

---
+++ LGU.WindowsUI/Views/HumanResource/EmployeeFingerPrintEnrollmentView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using LGU.ViewModels.HumanResource;
+using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource
+{
+    /// <summary>
+    /// Interaction logic for EmployeeFingerPrintEnrollmentView
+    /// </summary>
+    public partial class EmployeeFingerPrintEnrollmentView : UserControl
+    {
+        public EmployeeFingerPrintEnrollmentView()
+        {
+            InitializeComponent();
+            Loaded += (s, e) => ViewModel.Load();
+        }
+
+        public EmployeeFingerPrintEnrollmentViewModel ViewModel => DataContext as EmployeeFingerPrintEnrollmentViewModel;
+    }
+}
---
+++ LGU.WindowsUI/Views/HumanResource/PreviewEmployeeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource
+{
+    /// <summary>
+    /// Interaction logic for PreviewEmployeeView
+    /// </summary>
+    public partial class PreviewEmployeeView : UserControl
+    {
+        public PreviewEmployeeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
