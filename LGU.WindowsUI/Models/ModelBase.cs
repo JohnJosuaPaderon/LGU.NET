d14d0b40c21f75e5c2aaaa34113dc2a8a4e142c7
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-06 06:18:40 +0800

    Added: ModelBase.cs

---
+++ LGU.WindowsUI/Models/ModelBase.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Mvvm;
+
+namespace LGU.Models
+{
+    public abstract class ModelBase<T> : BindableBase
+    {
+        public ModelBase(T source)
+        {
+            Source = source;
+        }
+
+        protected T Source { get; }
+    }
+}
