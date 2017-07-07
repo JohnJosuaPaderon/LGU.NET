256a5d04df042a796f90f8a42afd681299bc0b10
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-07 14:43:06 +0800

    Added: IServiceCollectionExtension class

---
+++ LGU/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using LGU.EntityManagers;
+using LGU.EntityProcesses;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LGU.Extensions
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection UseSqlServer(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient<IGetSystemDate, GetSystemDate>();
+            serviceCollection.AddTransient<ISystemManager, SystemManager>();
+            return serviceCollection;
+        }
+    }
+}
