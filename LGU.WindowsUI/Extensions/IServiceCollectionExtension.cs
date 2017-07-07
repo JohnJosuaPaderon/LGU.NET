ed0b9343c6dc8adb386228ef9bc33c84bf71a5bd
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-07 14:45:55 +0800

    Added: IServiceCollectionExtension.cs

---
+++ LGU.WindowsUI/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LGU.Extensions
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection SetConnectionStringSource(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IConnectionStringSource, ConnectionStringSource>();
+            return serviceCollection;
+        }
+    }
+}
