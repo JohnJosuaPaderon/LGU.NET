f4a5d79986b0a6fd6a0110a45b6d47536376732e
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-07 14:42:07 +0800

    Added: ISystemManager interface; SystemManager class

---
+++ LGU/EntityManagers/ISystemManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityManagers
+{
+    public interface ISystemManager
+    {
+        IDataProcessResult<DateTime> GetSystemDate();
+        Task<IDataProcessResult<DateTime>> GetSystemDateAsync();
+        Task<IDataProcessResult<DateTime>> GetSystemDateAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ LGU/EntityManagers/SystemManager.cs
@@ -0,0 +1,32 @@
+﻿using LGU.EntityProcesses;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityManagers
+{
+    public sealed class SystemManager : ISystemManager
+    {
+        private readonly IGetSystemDate GetSystemDateProc;
+
+        public SystemManager(IGetSystemDate getSystemDate)
+        {
+            GetSystemDateProc = getSystemDate;
+        }
+
+        public IDataProcessResult<DateTime> GetSystemDate()
+        {
+            return GetSystemDateProc.Execute();
+        }
+
+        public Task<IDataProcessResult<DateTime>> GetSystemDateAsync(CancellationToken cancellationToken)
+        {
+            return GetSystemDateProc.ExecuteAsync(cancellationToken);
+        }
+
+        public Task<IDataProcessResult<DateTime>> GetSystemDateAsync()
+        {
+            return GetSystemDateProc.ExecuteAsync();
+        }
+    }
+}
