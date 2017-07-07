2db9d8ac5a4f932f2512ddc4ec9f3a30740ea091
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-07 14:43:42 +0800

    Added: Compressor.cs; RandomBytes.cs

---
+++ LGU/Utilities/RandomBytes.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography;
+
+namespace LGU.Utilities
+{
+    public static class RandomBytes
+    {
+        public static byte[] Get()
+        {
+            byte[] bytes = new byte[SaltLength];
+            RandomNumberGenerator.Create().GetBytes(bytes);
+            return bytes;
+        }
+
+        public static int SaltLength
+        {
+            get { return 8; }
+        }
+    }
+}
