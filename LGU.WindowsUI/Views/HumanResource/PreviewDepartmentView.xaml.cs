36da97887c131605194d12d11e3adb2ef9b4324f
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-07 14:48:19 +0800

    Added: PreviewDepartmentView.xaml; PreviewDepartmentView.xaml.cs Updated: EmployeeFingerPrintEnrollmentView.xaml; PreviewEmployeeView.xaml

---
+++ LGU.WindowsUI/Views/HumanResource/PreviewDepartmentView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource
+{
+    /// <summary>
+    /// Interaction logic for PreviewDepartmentView
+    /// </summary>
+    public partial class PreviewDepartmentView : UserControl
+    {
+        public PreviewDepartmentView()
+        {
+            InitializeComponent();
+        }
+    }
+}
