e676fd17043a8b695b3be90cd6f7c6d0b2de67a9
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-09 00:16:22 +0800

    Added: HumanResourceProcessBase.cs

---
+++ LGU/EntityProcesses/HumanResource/HumanResourceProcessBase.cs
@@ -0,0 +1,9 @@
+﻿namespace LGU.EntityProcesses.HumanResource
+{
+    public abstract class HumanResourceProcessBase : ProcessBase
+    {
+        public HumanResourceProcessBase(IConnectionStringSource connectionStringSource) : base(connectionStringSource, "HumanResource")
+        {
+        }
+    }
+}
