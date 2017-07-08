04e9953b9671fb9affac22670cf7a24d21b63971
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-09 00:20:00 +0800

    Added: NullableToBooleanConverter.cs

---
+++ LGU.WindowsUI/Converters/NullableToBooleanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LGU.Converters
+{
+    public sealed class NullableToBooleanConverter : IValueConverter
+    {
+        public static NullableToBooleanConverter Instance { get; } = new NullableToBooleanConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
