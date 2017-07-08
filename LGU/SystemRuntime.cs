896aba370b27660e0460e0f8a7788facdfcfd57a
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-09 00:19:26 +0800

    Added: SystemRuntime.cs; Updated: LGU.csproj; Removed: ServiceProvider.cs

---
+++ LGU/SystemRuntime.cs
@@ -0,0 +1,40 @@
+﻿using LGU.Entities.Core;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+
+namespace LGU
+{
+    public static class SystemRuntime
+    {
+        public static void Instantiate(IServiceCollection serviceCollection)
+        {
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+        }
+
+        public static IServiceProvider ServiceProvider { get; private set; }
+        public static string SystemDirectory { get; set; }
+        public static User CurrentUser { get; set; }
+
+        public static string ResolveSystemPath(string path)
+        {
+            if (path.StartsWith("~"))
+            {
+                path = path.Substring(2, path.Length - 2);
+                return Path.Combine(SystemDirectory, path);
+            }
+            else
+            {
+                return path;
+            }
+        }
+
+        public static string LogByInfo
+        {
+            get
+            {
+                return CurrentUser?.DisplayName ?? "UNDEFINED";
+            }
+        }
+    }
+}
