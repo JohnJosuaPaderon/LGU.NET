9acffd01f3b1e33e6f6e4011020a5bb6d9d650b7
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-09 22:19:31 +0800

    Added: AddEditEmployeeDialogView.xaml; AddEditEmployeeDialogView.xaml.cs Updated: AddEditDepartmentDialogView.xaml

---
+++ LGU.WindowsUI/Views/HumanResource/Dialogs/AddEditEmployeeDialogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for AddEditEmployeeDialogView
+    /// </summary>
+    public partial class AddEditEmployeeDialogView : UserControl
+    {
+        public AddEditEmployeeDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
