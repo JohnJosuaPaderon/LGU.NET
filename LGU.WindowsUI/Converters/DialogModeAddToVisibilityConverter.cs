a64b2ff4af6ec1f733c535ac2c938b20306ebb3c
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-09 22:17:12 +0800

    Added: DialogModeAddToVisibilityConverter.cs;

---
+++ LGU.WindowsUI/Converters/DialogModeAddToVisibilityConverter.cs
@@ -0,0 +1,23 @@
+﻿using LGU.ViewModels;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LGU.Converters
+{
+    public sealed class DialogModeAddToVisibilityConverter : IValueConverter
+    {
+        public static DialogModeAddToVisibilityConverter Instance { get; } = new DialogModeAddToVisibilityConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (DialogMode)value == DialogMode.Add ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
