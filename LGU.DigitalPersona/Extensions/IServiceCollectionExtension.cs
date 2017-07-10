bce19dc5471058cd2d57c8ab842ec73266ab47d8
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-10 17:28:24 +0800

    Added: IServiceCollectionExtension.cs

---
+++ LGU.DigitalPersona/Extensions/IServiceCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using LGU.EntityManagers.HumanResource;
+using LGU.EntityProcesses.HumanResource;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LGU.Extensions
+{
+    public static class IServiceCollectionExtension
+    {
+        public static IServiceCollection UseDigitalPersona(this IServiceCollection serviceCollection)
+        {
+            #region EmployeeFingerPrintSetManager
+            serviceCollection.AddTransient<IDeleteEmployeeFingerPrintSet, DeleteEmployeeFingerPrintSet>();
+            serviceCollection.AddTransient<IGetEmployeeFingerPrintSetList, GetEmployeeFingerPrintSetList>();
+            serviceCollection.AddTransient<IInsertEmployeeFingerPrintSet, InsertEmployeeFingerPrintSet>();
+            serviceCollection.AddTransient<IUpdateEmployeeFingerPrintSet, UpdateEmployeeFingerPrintSet>();
+            serviceCollection.AddSingleton<IEmployeeFingerPrintSetManager, EmployeeFingerPrintSetManager>();
+            #endregion
+
+            return serviceCollection;
+        }
+    }
+}
