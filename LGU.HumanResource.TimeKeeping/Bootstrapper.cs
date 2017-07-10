4280d7b3ec3a47c09e7305189079259ef3d3460a
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-10 17:30:11 +0800

    Added: Bootstrapper.cs; Updated: App.config; App.xaml.cs; LGU.HumanResource.TimeKeeping.csproj

---
+++ LGU.HumanResource.TimeKeeping/Bootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using LGU.Extensions;
+using LGU.Views.HumanResource;
+using Prism.Unity;
+
+namespace LGU.HumanResource
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterTypeForNavigation<LauncherView>();
+            Container.RegisterTypeForNavigation<PreviewEmployeeView>();
+            Container.RegisterTypeForNavigation<DepartmentManagementView>();
+            Container.RegisterTypeForNavigation<MainView>();
+            Container.RegisterTypeForNavigation<EmployeeFingerPrintEnrollmentView>();
+            Container.RegisterTypeForNavigation<TimeKeepingView>();
+        }
+
+        protected override void InitializeServices()
+        {
+            ServiceCollection.SetConnectionStringSource();
+            ServiceCollection.UseSqlServer();
+            ServiceCollection.UseDigitalPersona();
+        }
+    }
+}
