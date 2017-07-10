fcf8bc99a007f275d31a6e4828b5c214aad6c1e1
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-10 17:29:11 +0800

    Added: app.config; FingerPrintScannerHelper.cs; LGU.DigitalPersona.csproj; packages.config

---
+++ LGU.DigitalPersona/FingerPrintScannerHelper.cs
@@ -0,0 +1,32 @@
+﻿using DPFP;
+using DPFP.Capture;
+using DPFP.Processing;
+using System.IO;
+
+namespace LGU
+{
+    public static class FingerPrintScannerHelper
+    {
+        public static FeatureSet ExtractFeatures(Sample sample, DataPurpose purpose)
+        {
+            var extraction = new FeatureExtraction();
+            var feedback = CaptureFeedback.None;
+            var features = new FeatureSet();
+            extraction.CreateFeatureSet(sample, purpose, ref feedback, ref features);
+
+            if (feedback == CaptureFeedback.Good)
+            {
+                return features;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static Template ConvertFromStream(Stream stream)
+        {
+            return stream != null ? new Template(stream) : null;
+        }
+    }
+}
