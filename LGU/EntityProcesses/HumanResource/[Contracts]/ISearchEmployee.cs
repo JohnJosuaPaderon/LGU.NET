0da132bead7a0306fc793ceac8b4b4739dbf9cf9
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-10 17:24:23 +0800

    Added: ISearchEmployee.cs;

---
+++ LGU/EntityProcesses/HumanResource/[Contracts]/ISearchEmployee.cs
@@ -0,0 +1,9 @@
+﻿using LGU.Entities.HumanResource;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public interface ISearchEmployee : IEnumerableDataProcess<Employee>
+    {
+        string SearchKey { get; set; }
+    }
+}
