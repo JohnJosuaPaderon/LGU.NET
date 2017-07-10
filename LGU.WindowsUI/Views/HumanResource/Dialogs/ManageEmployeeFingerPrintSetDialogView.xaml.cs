f03237ee8daf2c6cd6daeb149c509feef5994f0b
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-10 17:34:21 +0800

    Added: ManageEmployeeFingerPrintSetDialogView.xaml; ManageEmployeeFingerPrintSetDialogView.xaml.cs;

---
+++ LGU.WindowsUI/Views/HumanResource/Dialogs/ManageEmployeeFingerPrintSetDialogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for ManageEmployeeFingerPrintSetDialogView
+    /// </summary>
+    public partial class ManageEmployeeFingerPrintSetDialogView : UserControl
+    {
+        public ManageEmployeeFingerPrintSetDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
