89967fe102b8f12db7dee80f1f9f3499d972cfc2
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-10 17:27:47 +0800

    Added: DeleteEmployeeFingerPrintSet.cs; GetEmployeeFingerPrintSetList.cs; InsertEmployeeFingerPrintSet.cs; UpdateEmployeeFingerPrintSet.cs;

---
+++ LGU.DigitalPersona/EntityProcesses/HumanResource/[Implementations]/GetEmployeeFingerPrintSetList.cs
@@ -0,0 +1,44 @@
+﻿using LGU.Data.RDBMS;
+using LGU.Entities.HumanResource;
+using LGU.EntityProcessHelpers.HumanResource;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public sealed class GetEmployeeFingerPrintSetList : HumanResourceProcessBase, IGetEmployeeFingerPrintSetList
+    {
+        public GetEmployeeFingerPrintSetList(IConnectionStringSource connectionStringSource) : base(connectionStringSource)
+        {
+        }
+
+        private SqlQueryInfo QueryInfo
+        {
+            get
+            {
+                return SqlQueryInfo.CreateProcedureQueryInfo(GetQualifiedDbObjectName("GetEmployeeFingerPrintSetList"), GetProcessResult);
+            }
+        }
+
+        private IProcessResult GetProcessResult(SqlCommand command, int affectedRows)
+        {
+            return new ProcessResult(ProcessResultStatus.Success);
+        }
+
+        public IEnumerableDataProcessResult<EmployeeFingerPrintSet> Execute()
+        {
+            return SqlHelper.ExecuteReaderEnumerable(QueryInfo, EmployeeFingerPrintSetProcessHelper.EnumerableFromReader);
+        }
+
+        public Task<IEnumerableDataProcessResult<EmployeeFingerPrintSet>> ExecuteAsync()
+        {
+            return SqlHelper.ExecuteReaderEnumerableAsync(QueryInfo, EmployeeFingerPrintSetProcessHelper.EnumerableFromReaderAsync);
+        }
+
+        public Task<IEnumerableDataProcessResult<EmployeeFingerPrintSet>> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return SqlHelper.ExecuteReaderEnumerableAsync(QueryInfo, EmployeeFingerPrintSetProcessHelper.EnumerableFromReaderAsync, cancellationToken);
+        }
+    }
+}
---
+++ LGU.DigitalPersona/EntityProcesses/HumanResource/[Implementations]/InsertEmployeeFingerPrintSet.cs
@@ -0,0 +1,67 @@
+﻿using LGU.Data.Extensions;
+using LGU.Data.RDBMS;
+using LGU.Entities.HumanResource;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public sealed class InsertEmployeeFingerPrintSet : HumanResourceProcessBase, IInsertEmployeeFingerPrintSet
+    {
+        public InsertEmployeeFingerPrintSet(IConnectionStringSource connectionStringSource) : base(connectionStringSource)
+        {
+        }
+
+        public EmployeeFingerPrintSet FingerPrintSet { get; set; }
+
+        private SqlDataQueryInfo<EmployeeFingerPrintSet> QueryInfo
+        {
+            get
+            {
+                return SqlDataQueryInfo<EmployeeFingerPrintSet>.CreateProcedureQueryInfo(FingerPrintSet, GetQualifiedDbObjectName("InsertEmployeeFingerPrintSet"), GetProcessResult, true)
+                    .AddInputParameter("@_Id", FingerPrintSet.Employee?.Id)
+                    .AddInputParameter("@_LeftThumb", FingerPrintSet.LeftThumb?.Data?.Bytes, SqlDbType.VarBinary)
+                    .AddInputParameter("@_LeftIndexFinger", FingerPrintSet.LeftIndexFinger?.Data?.Bytes, SqlDbType.VarBinary)
+                    .AddInputParameter("@_LeftMiddleFinger", FingerPrintSet.LeftMiddleFinger?.Data?.Bytes, SqlDbType.VarBinary)
+                    .AddInputParameter("@_LeftRingFinger", FingerPrintSet.LeftRingFinger?.Data?.Bytes, SqlDbType.VarBinary)
+                    .AddInputParameter("@_LeftLittleFinger", FingerPrintSet.LeftLittleFinger?.Data?.Bytes, SqlDbType.VarBinary)
+                    .AddInputParameter("@_RightThumb", FingerPrintSet.RightThumb?.Data?.Bytes, SqlDbType.VarBinary)
+                    .AddInputParameter("@_RightIndexFinger", FingerPrintSet.RightIndexFinger?.Data?.Bytes, SqlDbType.VarBinary)
+                    .AddInputParameter("@_RightMiddleFinger", FingerPrintSet.RightMiddleFinger?.Data?.Bytes, SqlDbType.VarBinary)
+                    .AddInputParameter("@_RightRingFinger", FingerPrintSet.RightRingFinger?.Data?.Bytes, SqlDbType.VarBinary)
+                    .AddInputParameter("@_RightLittleFinger", FingerPrintSet.RightLittleFinger?.Data?.Bytes, SqlDbType.VarBinary)
+                    .AddLogByParameter();
+            }
+        }
+
+        private IDataProcessResult<EmployeeFingerPrintSet> GetProcessResult(EmployeeFingerPrintSet data, SqlCommand command, int affectedRows)
+        {
+            if (affectedRows == 1)
+            {
+                return new DataProcessResult<EmployeeFingerPrintSet>(data, ProcessResultStatus.Success);
+            }
+            else
+            {
+                return new DataProcessResult<EmployeeFingerPrintSet>(ProcessResultStatus.Failed);
+            }
+
+        }
+
+        public IDataProcessResult<EmployeeFingerPrintSet> Execute()
+        {
+            return SqlHelper.ExecuteNonQuery(QueryInfo);
+        }
+
+        public Task<IDataProcessResult<EmployeeFingerPrintSet>> ExecuteAsync()
+        {
+            return SqlHelper.ExecuteNonQueryAsync(QueryInfo);
+        }
+
+        public Task<IDataProcessResult<EmployeeFingerPrintSet>> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return SqlHelper.ExecuteNonQueryAsync(QueryInfo, cancellationToken);
+        }
+    }
+}
