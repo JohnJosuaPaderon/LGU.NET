a9305e4960a215d52ee6d5989de5c2589d5ca115
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-11 19:02:01 +0800

    Added: IGetEmployeeFingerPrintSetById.cs;

---
+++ LGU/EntityProcesses/HumanResource/[Contracts]/IGetEmployeeFingerPrintSetById.cs
@@ -0,0 +1,9 @@
+﻿using LGU.Entities.HumanResource;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public interface IGetEmployeeFingerPrintSetById : IDataProcess<EmployeeFingerPrintSet>
+    {
+        Employee Employee { get; set; }
+    }
+}
