93333d1681f779accc32f057bffd6849d0a4737c
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-12 23:32:18 +0800

    Added: SecureHash.cs

---
+++ LGU/Security/SecureHash.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace LGU.Security
+{
+    public static class SecureHash
+    {
+        private static string ComputeHash(string value, HashAlgorithm algorithm)
+        {
+            using (algorithm)
+            {
+                var computedHash = algorithm.ComputeHash(Encoding.UTF8.GetBytes(value));
+                return BitConverter.ToString(computedHash).Replace("-", string.Empty);
+            }
+        }
+
+        public static string ComputeSHA256(string value)
+        {
+            return ComputeHash(value, SHA256.Create());
+        }
+
+        public static string ComputeSHA512(string value)
+        {
+            return ComputeHash(value, SHA512.Create());
+        }
+    }
+}
