983db5cfb46bd630f081f060e2e354da9a7824bd
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-12 23:36:37 +0800

    Added: App.config; App.xaml; App.xaml.cs; Bootstrapper.cs; LGU.SystemAdministration.csproj; logo.ico; packages.config

---
+++ LGU.SystemAdministration/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using LGU.ViewModels;
+using LGU.Views.SystemAdministration;
+using System.Windows;
+
+namespace LGU.SystemAdministration
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindowViewModel.InitialMainContentRegionSource = nameof(LauncherView);
+            new Bootstrapper().Run();
+        }
+    }
+}
---
+++ LGU.SystemAdministration/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using LGU.Extensions;
+using LGU.Views.SystemAdministration;
+using Prism.Unity;
+
+namespace LGU.SystemAdministration
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterTypeForNavigation<LauncherView>();
+            Container.RegisterTypeForNavigation<MainView>();
+        }
+
+        protected override void InitializeServices()
+        {
+            base.InitializeServices();
+
+            ServiceCollection.SetSystemAdministratorManager();
+        }
+    }
+}
