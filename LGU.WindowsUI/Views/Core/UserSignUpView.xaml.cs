e328385d93ccc7b8263a934a0cf866c239852c33
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-12 23:39:25 +0800

    Added: UserSignUpView.xaml; UserSignUpView.xaml.cs

---
+++ LGU.WindowsUI/Views/Core/UserSignUpView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.Core
+{
+    /// <summary>
+    /// Interaction logic for UserSignUpView
+    /// </summary>
+    public partial class UserSignUpView : UserControl
+    {
+        public UserSignUpView()
+        {
+            InitializeComponent();
+        }
+    }
+}
