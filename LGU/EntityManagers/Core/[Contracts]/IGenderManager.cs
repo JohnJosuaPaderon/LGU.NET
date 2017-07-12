e197e8fa38caf0b160728508f020ce7c7399e24b
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-12 23:26:34 +0800

    Added: IGenderManager.cs; IPersonManager.cs; IPersonNameManager.cs; IUserManager.cs;

---
+++ LGU/EntityManagers/Core/[Contracts]/IGenderManager.cs
@@ -0,0 +1,16 @@
+﻿using LGU.Entities.Core;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityManagers.Core
+{
+    public interface IGenderManager
+    {
+        IDataProcessResult<Gender> GetById(short id);
+        Task<IDataProcessResult<Gender>> GetByIdAsync(short id);
+        Task<IDataProcessResult<Gender>> GetByIdAsync(short id, CancellationToken cancellationToken);
+        IEnumerableDataProcessResult<Gender> GetList();
+        Task<IEnumerableDataProcessResult<Gender>> GetListAsync();
+        Task<IEnumerableDataProcessResult<Gender>> GetListAsync(CancellationToken cancellationToken);
+    }
+}
