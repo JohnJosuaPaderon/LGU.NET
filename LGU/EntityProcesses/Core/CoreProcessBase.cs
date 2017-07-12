d68050c645753902a1dd67ce9672709ad6b25238
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-12 23:31:25 +0800

    Added: CoreProcessBase.cs;

---
+++ LGU/EntityProcesses/Core/CoreProcessBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LGU.EntityProcesses.Core
+{
+    public abstract class CoreProcessBase : ProcessBase
+    {
+        public CoreProcessBase(IConnectionStringSource connectionStringSource) : base(connectionStringSource, "Core")
+        {
+        }
+    }
+}
