5a2b302d44610deaaf095d222e706eeb0ac4e3fb
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-12 23:27:56 +0800

    Added: IDepartmentManager.cs; IEmployeeFingerPrintSetManager.cs; IEmployeeManager.cs;

---
+++ LGU/EntityManagers/HumanResource/[Contracts]/IDepartmentManager.cs
@@ -0,0 +1,14 @@
+﻿using LGU.Entities;
+using LGU.Entities.HumanResource;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityManagers.HumanResource
+{
+    public interface IDepartmentManager : IEntityManager<Department, int>
+    {
+        IEnumerableDataProcessResult<Department> Search(string searchKey);
+        Task<IEnumerableDataProcessResult<Department>> SearchAsync(string searchKey);
+        Task<IEnumerableDataProcessResult<Department>> SearchAsync(string searchKey, CancellationToken cancellationToken);
+    }
+}
---
+++ LGU/EntityManagers/HumanResource/[Contracts]/IEmployeeFingerPrintSetManager.cs
@@ -0,0 +1,13 @@
+﻿using LGU.Entities.HumanResource;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityManagers.HumanResource
+{
+    public interface IEmployeeFingerPrintSetManager : IDataManager<EmployeeFingerPrintSet>
+    {
+        IDataProcessResult<EmployeeFingerPrintSet> GetById(Employee employee);
+        Task<IDataProcessResult<EmployeeFingerPrintSet>> GetByIdAsync(Employee employee);
+        Task<IDataProcessResult<EmployeeFingerPrintSet>> GetByIdAsync(Employee employee, CancellationToken cancellationToken);
+    }
+}
