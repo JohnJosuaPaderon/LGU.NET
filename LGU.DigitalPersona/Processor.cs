5ce1c196dcb0a170270febd74e818f7a2f9bfa05
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-14 14:46:46 +0800

    Added: Processor.cs; Verifier.cs; Updated: LGU.DigitalPersona.csproj

---
+++ LGU.DigitalPersona/Processor.cs
@@ -0,0 +1,47 @@
+﻿using DPFP;
+using DPFP.Capture;
+using System;
+
+namespace LGU
+{
+    public abstract class Processor : DPFP.Capture.EventHandler
+    {
+        public Processor(Action<string> messageInvoker)
+        {
+            MessageInvoker = messageInvoker;
+        }
+
+        private readonly Action<string> MessageInvoker;
+        public abstract void ProcessSample(Sample sample);
+
+        public void OnComplete(object capture, string readerSerialNumber, Sample sample)
+        {
+            MessageInvoker("The finger print sample was captured.");
+        }
+
+        public void OnFingerGone(object capture, string readerSerialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnFingerTouch(object capture, string readerSerialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnReaderConnect(object capture, string readerSerialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnReaderDisconnect(object capture, string readerSerialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSampleQuality(object capture, string readerSerialNumber, CaptureFeedback captureFeedback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LGU.DigitalPersona/Verifier.cs
@@ -0,0 +1,46 @@
+﻿using DPFP;
+using DPFP.Capture;
+using System;
+
+namespace LGU
+{
+    public sealed class Verifier : DPFP.Capture.EventHandler
+    {
+        public Verifier(Action<string> messageInvoker)
+        {
+            MessageInvoker = messageInvoker;
+        }
+
+        private readonly Action<string> MessageInvoker;
+
+        public void OnComplete(object Capture, string ReaderSerialNumber, Sample Sample)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnFingerGone(object Capture, string ReaderSerialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnFingerTouch(object Capture, string ReaderSerialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnReaderConnect(object Capture, string ReaderSerialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnReaderDisconnect(object Capture, string ReaderSerialNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnSampleQuality(object Capture, string ReaderSerialNumber, CaptureFeedback CaptureFeedback)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
