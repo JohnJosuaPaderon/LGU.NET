01ea21f7dad306c732b2cfb33a6bbffd66030b74
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-16 18:16:35 +0800

    Added: ITimeLogManager.cs; ITimeLogTypeManager.cs;

---
+++ LGU/EntityManagers/HumanResource/[Contracts]/ITimeLogManager.cs
@@ -0,0 +1,14 @@
+﻿using LGU.Entities;
+using LGU.Entities.HumanResource;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityManagers.HumanResource
+{
+    public interface ITimeLogManager : IEntityManager<TimeLog, long>
+    {
+        IDataProcessResult<TimeLog> Log(Employee employee);
+        Task<IDataProcessResult<TimeLog>> LogAsync(Employee employee);
+        Task<IDataProcessResult<TimeLog>> LogAsync(Employee employee, CancellationToken cancellationToken);
+    }
+}
