b08b8cd38fc9d66d7d21b1cca43baeb7838482ad
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-16 15:36:36 +0800

    Added: IStaticEntityManager.cs;

---
+++ LGU/Entities/IStaticEntityManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.Entities
+{
+    public interface IStaticEntityManager<T, TIdentifier>
+        where T : IEntity<TIdentifier>
+    {
+        IDataProcessResult<T> GetById(TIdentifier id);
+        Task<IDataProcessResult<T>> GetByIdAsync(TIdentifier id);
+        Task<IDataProcessResult<T>> GetByIdAsync(TIdentifier id, CancellationToken cancellationToken);
+        IEnumerableDataProcessResult<T> GetList();
+        Task<IEnumerableDataProcessResult<T>> GetListAsync();
+        Task<IEnumerableDataProcessResult<T>> GetListAsync(CancellationToken cancellationToken);
+    }
+}
