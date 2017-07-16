681c31562689f1691ed231352fe57d67ecb9abfa
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-16 18:17:17 +0800

    Added:ManagerBase.cs;

---
+++ LGU/EntityManagers/ManagerBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace LGU.EntityManagers
+{
+    public abstract class ManagerBase
+    {
+        protected void InvokeIfSuccess(ProcessResultStatus status, Action expression)
+        {
+            if (status == ProcessResultStatus.Success)
+            {
+                expression();
+            }
+        }
+
+        protected void InvokeIfSuccessAndListNotEmpty<T>(IEnumerableDataProcessResult<T> result, Action<T> expression)
+        {
+            if (result.Status == ProcessResultStatus.Success && result.DataList != null && result.DataList.Any())
+            {
+                foreach (var item in result.DataList)
+                {
+                    expression(item);
+                }
+            }
+        }
+    }
+}
