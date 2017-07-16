0ecf41eff348e46d8b5e743dae3fbde79e606ed5
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-16 18:12:48 +0800

    Added: TimeLog.cs; TimeLogType.cs;

---
+++ LGU/Entities/HumanResource/TimeLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LGU.Entities.HumanResource
+{
+    public class TimeLog : Entity<long>
+    {
+        public TimeLog(Employee employee)
+        {
+            Employee = employee ?? throw new ArgumentNullException(nameof(employee));
+        }
+
+        public Employee Employee { get; }
+        public DateTime? LoginDate { get; set; }
+        public DateTime? LogoutDate { get; set; }
+        public TimeLogType Type { get; set; }
+    }
+}
---
+++ LGU/Entities/HumanResource/TimeLogType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LGU.Entities.HumanResource
+{
+    public class TimeLogType : Entity<short>
+    {
+        public string Description { get; set; }
+        public TimeSpan WorkTimeLength { get; set; }
+        public TimeSpan? BreakTimeLength { get; set; }
+
+        public override string ToString()
+        {
+            return Description;
+        }
+    }
+}
