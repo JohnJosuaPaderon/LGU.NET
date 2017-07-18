eb493c41e2dd5beb7051903c3ebd9722e4f7df2a
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-18 10:31:09 +0800

    Various updates.

---
+++ LGU.WindowsUI/ViewModels/Core/UserSignUpViewModel.cs
@@ -0,0 +1,33 @@
+﻿using LGU.EntityManagers.Core;
+using LGU.Models.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Prism.Commands;
+using Prism.Events;
+using Prism.Regions;
+
+namespace LGU.ViewModels.Core
+{
+    public sealed class UserSignUpViewModel : ViewModelBase
+    {
+        private readonly IUserManager UserManager;
+
+        public UserSignUpViewModel(IRegionManager regionManager, IEventAggregator eventAggregator) : base(regionManager, eventAggregator)
+        {
+            UserManager = SystemRuntime.Services.GetService<IUserManager>();
+            SaveCommand = new DelegateCommand(Save);
+        }
+
+        public DelegateCommand SaveCommand { get; }
+
+        private UserSignUpModel _User;
+        public UserSignUpModel User
+        {
+            get { return _User; }
+            set { SetProperty(ref _User, value); }
+        }
+
+        private void Save()
+        {
+        }
+    }
+}
