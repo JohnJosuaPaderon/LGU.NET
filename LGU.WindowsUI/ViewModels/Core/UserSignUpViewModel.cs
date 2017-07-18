34c163faeb12c94ef0a9a4b77e1e3d04008a9091
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-19 07:42:06 +0800

    Updated: UserSignUpViewModel.cs

--- LGU.WindowsUI/ViewModels/Core/UserSignUpViewModel.cs
+++ LGU.WindowsUI/ViewModels/Core/UserSignUpViewModel.cs
@@ -1,13 +1,17 @@
-﻿using LGU.EntityManagers.Core;
+﻿using LGU.Entities.Core;
+using LGU.EntityManagers.Core;
 using LGU.Models.Core;
+using LGU.Processes;
+using LGU.Utilities;
 using Microsoft.Extensions.DependencyInjection;
 using Prism.Commands;
 using Prism.Events;
 using Prism.Regions;
+using System.Threading.Tasks;
 
 namespace LGU.ViewModels.Core
 {
-    public sealed class UserSignUpViewModel : ViewModelBase
+    public sealed class UserSignUpViewModel : DialogViewModelBase
     {
         private readonly IUserManager UserManager;
 
@@ -15,6 +19,7 @@ namespace LGU.ViewModels.Core
         {
             UserManager = SystemRuntime.Services.GetService<IUserManager>();
             SaveCommand = new DelegateCommand(Save);
+            User = new UserSignUpModel(new User());
         }
 
         public DelegateCommand SaveCommand { get; }
@@ -26,8 +31,50 @@ namespace LGU.ViewModels.Core
             set { SetProperty(ref _User, value); }
         }
 
-        private void Save()
+        private async void Save()
         {
+            if (string.IsNullOrWhiteSpace(User.Password))
+            {
+                MessageLog = "Password is invalid.";
+            }
+            else if (User.Password != User.ConfirmPassword)
+            {
+                MessageLog = "Password not matched.";
+            }
+            else if (await IsUsernameExistsAsync())
+            {
+                MessageLog = "Username already exists.";
+            }
+            else
+            {
+                var result = await UserManager.InsertAsync(User.GetSource());
+
+                if (result.Status == ProcessResultStatus.Success)
+                {
+                    User = new UserSignUpModel(new User());
+                    DialogHelper.CloseDialog();
+                    ShowInfoMessage("User has been added successfully.");
+                }
+                else
+                {
+                    MessageLog = result.Message;
+                }
+            }
+        }
+
+        private async Task<bool> IsUsernameExistsAsync()
+        {
+            var result = await UserManager.IsUsernameExistsAsync(SecureStringConverter.Convert(User.Username));
+
+            if (result.Status == ProcessResultStatus.Success)
+            {
+                return result.Data;
+            }
+            else
+            {
+                MessageLog = result.Message;
+                return false;
+            }
         }
     }
 }
