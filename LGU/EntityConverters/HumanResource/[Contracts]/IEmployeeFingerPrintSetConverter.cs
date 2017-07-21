ad8dd4be9d74c5de643cc078de890bddf1a3f2e5
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-21 09:03:22 +0800

    Added: IEmployeeFingerPrintSetConverter.cs

---
+++ LGU/EntityConverters/HumanResource/[Contracts]/IEmployeeFingerPrintSetConverter.cs
@@ -0,0 +1,11 @@
+﻿using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System.Data.Common;
+
+namespace LGU.EntityConverters.HumanResource
+{
+    public interface IEmployeeFingerPrintSetConverter<TDataReader> : IDataConverter<EmployeeFingerPrintSet, TDataReader>
+        where TDataReader : DbDataReader
+    {
+    }
+}
