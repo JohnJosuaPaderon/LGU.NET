05d900f3f043e11cf08c380556d31d88bf415048
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-21 09:11:50 +0800

    Updated: EmployeeFingerPrintSetProcess.cs;

---
+++ LGU.DigitalPersona/EntityProcesses/HumanResource/EmployeeFingerPrintSetProcess.cs
@@ -0,0 +1,15 @@
+﻿using LGU.EntityConverters.HumanResource;
+using System.Data.SqlClient;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public abstract class EmployeeFingerPrintSetProcess : HumanResourceProcessBase
+    {
+        protected readonly IEmployeeFingerPrintSetConverter<SqlDataReader> Converter;
+
+        public EmployeeFingerPrintSetProcess(IConnectionStringSource connectionStringSource, IEmployeeFingerPrintSetConverter<SqlDataReader> converter) : base(connectionStringSource)
+        {
+            Converter = converter;
+        }
+    }
+}
