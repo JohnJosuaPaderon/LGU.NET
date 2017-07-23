20ae8202c628cfba6ff9498c42397394adba48fc
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-23 23:04:21 +0800

    Updated: SqlQueryInfoExtension.cs; Removed: SqlDataQueryInfoExtension.cs;

--- LGU/Data/Extensions/SqlQueryInfoExtension.cs
+++ LGU/Data/Extensions/SqlQueryInfoExtension.cs
@@ -30,5 +30,35 @@ namespace LGU.Data.Extensions
             queryInfo.Parameters.AddInputOutput(parameterName, value);
             return queryInfo;
         }
+
+        public static SqlQueryInfo<T> AddInputParameter<T>(this SqlQueryInfo<T> queryInfo, string parameterName, object value)
+        {
+            queryInfo.Parameters.AddInput(parameterName, value);
+            return queryInfo;
+        }
+
+        public static SqlQueryInfo<T> AddInputParameter<T>(this SqlQueryInfo<T> queryInfo, string parameterName, object value, SqlDbType type)
+        {
+            queryInfo.Parameters.Add(new SqlParameter(parameterName, value ?? DBNull.Value) { SqlDbType = type });
+            return queryInfo;
+        }
+
+        public static SqlQueryInfo<T> AddOutputParameter<T>(this SqlQueryInfo<T> queryInfo, string parameterName, DbType dbType)
+        {
+            queryInfo.Parameters.AddOutput(parameterName, dbType);
+            return queryInfo;
+        }
+
+        public static SqlQueryInfo<T> AddInputOutputParameter<T>(this SqlQueryInfo<T> queryInfo, string parameterName, object value)
+        {
+            queryInfo.Parameters.AddInputOutput(parameterName, value);
+            return queryInfo;
+        }
+
+        public static SqlQueryInfo<T> AddLogByParameter<T>(this SqlQueryInfo<T> queryInfo)
+        {
+            queryInfo.Parameters.AddInput("@_LogBy", SystemRuntime.LogByInfo);
+            return queryInfo;
+        }
     }
 }
