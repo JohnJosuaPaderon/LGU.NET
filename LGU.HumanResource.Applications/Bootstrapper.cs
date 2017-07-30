e55282c5b65e99b80a260bc7a4a80e620de100f4
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-30 22:32:58 +0800

    Added: Bootstrapper.cs; logo.ico; Updated: App.config; App.xaml.cs; LGU.HumanResource.Applications.csproj

---
+++ LGU.HumanResource.Applications/Bootstrapper.cs
@@ -0,0 +1,16 @@
+﻿using LGU.Views.HumanResource;
+using Prism.Unity;
+
+namespace LGU.HumanResource.Applications
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterTypeForNavigation<MainView>();
+            Container.RegisterTypeForNavigation<TimeLogExportView>();
+        }
+    }
+}
