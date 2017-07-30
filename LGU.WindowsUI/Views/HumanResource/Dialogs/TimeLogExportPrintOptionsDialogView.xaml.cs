916393491b6aa4a63234afcff9bbed629c968cfb
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-30 22:35:37 +0800

    Aded: TimeLogExportPrintOptionsDialogVieew.xaml; TimeLogExportPrintOptionsDialogView.xaml.cs;

---
+++ LGU.WindowsUI/Views/HumanResource/Dialogs/TimeLogExportPrintOptionsDialogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for TimeLogExportPrintOptionsDialogView
+    /// </summary>
+    public partial class TimeLogExportPrintOptionsDialogView : UserControl
+    {
+        public TimeLogExportPrintOptionsDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
