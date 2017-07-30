b4d65ed2294c3ba93e918526919cc1fd570da112
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-30 22:32:23 +0800

    Updated: AssemblyInfo.cs;

--- LGU.HumanResource.Applications/Properties/AssemblyInfo.cs
+++ LGU.HumanResource.Applications/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LGU.HumanResource.Applications")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 John Josua R. Paderon")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
