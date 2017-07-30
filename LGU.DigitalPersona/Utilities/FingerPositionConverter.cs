3b1b0ba2674cd2b891facf1a2f47a7bb20de53e4
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-30 22:31:35 +0800

    Added: FingerPositionConverter.cs;

---
+++ LGU.DigitalPersona/Utilities/FingerPositionConverter.cs
@@ -0,0 +1,37 @@
+﻿using DPFP.ID;
+using LGU.Entities.Core;
+
+namespace LGU.Utilities
+{
+    public static class FingerPositionConverter
+    {
+        public static FingerPosition FromFingerPrint(FingerPrint fingerPrint)
+        {
+            switch (fingerPrint.HandType)
+            {
+                case HandType.Left:
+                    switch (fingerPrint.FingerType)
+                    {
+                        case FingerType.Thumb: return FingerPosition.LeftThumb;
+                        case FingerType.IndexFinger: return FingerPosition.LeftIndex;
+                        case FingerType.MiddleFinger: return FingerPosition.LeftMiddle;
+                        case FingerType.RingFinger: return FingerPosition.LeftRing;
+                        case FingerType.LittleFinger: return FingerPosition.LeftLittle;
+                    }
+                    break;
+                case HandType.Right:
+                    switch (fingerPrint.FingerType)
+                    {
+                        case FingerType.Thumb: return FingerPosition.RightThumb;
+                        case FingerType.IndexFinger: return FingerPosition.RightIndex;
+                        case FingerType.MiddleFinger: return FingerPosition.RightMiddle;
+                        case FingerType.RingFinger: return FingerPosition.RightRing;
+                        case FingerType.LittleFinger: return FingerPosition.RightLittle;
+                    }
+                    break;
+            }
+
+            return FingerPosition.LeftThumb;
+        }
+    }
+}
