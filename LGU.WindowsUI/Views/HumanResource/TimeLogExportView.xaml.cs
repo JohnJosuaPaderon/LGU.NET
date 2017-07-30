f8a0450dfcce867b8be85702f07c126de9d75ab7
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-30 22:36:04 +0800

    Added: TimeLogExportView.xaml; TimeLogExportView.xaml.cs; Updated: LauncherView.xaml

---
+++ LGU.WindowsUI/Views/HumanResource/TimeLogExportView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource
+{
+    /// <summary>
+    /// Interaction logic for TimeLogExportView
+    /// </summary>
+    public partial class TimeLogExportView : UserControl
+    {
+        public TimeLogExportView()
+        {
+            InitializeComponent();
+        }
+    }
+}
