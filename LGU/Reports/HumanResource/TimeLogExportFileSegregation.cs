892879e9b743ac4877f2722117b1d03242462556
johnpad53 <johnjosua.paderon@outlook.com>
2017-07-30 22:30:26 +0800

    Added: TimeLogExportFileSegregation.cs; TimeLogExportPrintOptions.cs;

---
+++ LGU/Reports/HumanResource/TimeLogExportFileSegregation.cs
@@ -0,0 +1,9 @@
+﻿namespace LGU.Reports.HumanResource
+{
+    public enum TimeLogExportFileSegregation
+    {
+        OneFile = 0,
+        MultipleFiles = 1,
+        MultipleFiles_Department = 2
+    }
+}
