44b125027a4346d58d1bd2ac3b0292826582fc42
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-03 19:25:33 +0800

    Added: TimeLogExportConfiguration.cs; TimeLogExportFileSegregation.cs; TimeLogExportPrintOptions.cs;

---
+++ LGU.Windows/Reports/HumanResource/TimeLogExportPrintOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace LGU.Reports.HumanResource
+{
+    public class TimeLogExportPrintOptions : ExcelExportPrintOptions
+    {
+        public TimeLogExportFileSegregation FileSegregation { get; set; }
+    }
+}
