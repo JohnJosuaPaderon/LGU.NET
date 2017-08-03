b5445a465fa2529fee52e615727fa68ef606283a
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-03 19:21:27 +0800

    Added: IGetUpdatedEmployeeFingerPrintSetList.cs;

---
+++ LGU/EntityProcesses/HumanResource/[Contracts]/IGetUpdatedEmployeeFingerPrintSetList.cs
@@ -0,0 +1,11 @@
+﻿using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public interface IGetUpdatedEmployeeFingerPrintSetList : IEnumerableProcess<EmployeeFingerPrintSet>
+    {
+        DateTime LogDate { get; set; }
+    }
+}
