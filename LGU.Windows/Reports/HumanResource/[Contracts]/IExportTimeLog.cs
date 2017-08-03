064f67b631247ddd55a66dde5f9922edb2f0e4c8
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-03 19:24:50 +0800

    Added: IExportTimeLog.cs;

---
+++ LGU.Windows/Reports/HumanResource/[Contracts]/IExportTimeLog.cs
@@ -0,0 +1,10 @@
+﻿using LGU.Entities.HumanResource;
+using System.Collections.Generic;
+
+namespace LGU.Reports.HumanResource
+{
+    public interface IExportTimeLog : IExport
+    {
+        IEnumerable<TimeLog> TimeLogs { get; }
+    }
+}
