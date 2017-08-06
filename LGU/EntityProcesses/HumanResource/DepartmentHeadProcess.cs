98fc4e8986176138bfb6675095f9d738b6ff95b3
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-06 22:41:10 +0800

    Added: DepartmentHeadProcess.cs; EmployeeWorkTimeScheduledProcess.cs;

---
+++ LGU/EntityProcesses/HumanResource/DepartmentHeadProcess.cs
@@ -0,0 +1,15 @@
+﻿using LGU.EntityConverters.HumanResource;
+using System.Data.SqlClient;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public abstract class DepartmentHeadProcess : HumanResourceProcessBase
+    {
+        protected readonly IDepartmentHeadConverter<SqlDataReader> r_Converter;
+
+        public DepartmentHeadProcess(IConnectionStringSource connectionStringSource, IDepartmentHeadConverter<SqlDataReader> converter) : base(connectionStringSource)
+        {
+            r_Converter = converter;
+        }
+    }
+}
