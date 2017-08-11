612daa51da94bd1f12ca86bece900685c93ec6d2
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-11 19:32:26 +0800

    Added: ValueRangeModel.cs;

---
+++ LGU.WindowsUI/Models/ValueRangeModel.cs
@@ -0,0 +1,30 @@
+﻿namespace LGU.Models
+{
+    public sealed class ValueRangeModel<T> : ModelBase<ValueRange<T>> 
+    {
+        public ValueRangeModel(ValueRange<T> source) : base(source)
+        {
+            Begin = source.Begin;
+            End = source.End;
+        }
+
+        private T _Begin;
+        public T Begin
+        {
+            get { return _Begin; }
+            set { SetProperty(ref _Begin, value); }
+        }
+
+        private T _End;
+        public T End
+        {
+            get { return _End; }
+            set { SetProperty(ref _End, value); }
+        }
+
+        public override ValueRange<T> GetSource()
+        {
+            return new ValueRange<T>(Begin, End);
+        }
+    }
+}
