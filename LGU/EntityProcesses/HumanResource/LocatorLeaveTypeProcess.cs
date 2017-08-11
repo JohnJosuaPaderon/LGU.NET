2771eefceb40091459fcaac9e27f1096b220fa0e
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-11 19:13:17 +0800

    Added: LocatorLeaveTypeProcess.cs; LocatorProcess.cs;

---
+++ LGU/EntityProcesses/HumanResource/LocatorLeaveTypeProcess.cs
@@ -0,0 +1,15 @@
+﻿using LGU.EntityConverters.HumanResource;
+using System.Data.SqlClient;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public abstract class LocatorLeaveTypeProcess : HumanResourceProcessBase
+    {
+        protected readonly ILocatorLeaveTypeConverter<SqlDataReader> r_Converter;
+
+        public LocatorLeaveTypeProcess(IConnectionStringSource connectionStringSource, ILocatorLeaveTypeConverter<SqlDataReader> converter) : base(connectionStringSource)
+        {
+            r_Converter = converter;
+        }
+    }
+}
---
+++ LGU/EntityProcesses/HumanResource/LocatorProcess.cs
@@ -0,0 +1,15 @@
+﻿using LGU.EntityConverters.HumanResource;
+using System.Data.SqlClient;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public abstract class LocatorProcess : HumanResourceProcessBase
+    {
+        protected readonly ILocatorConverter<SqlDataReader> r_Converter;
+
+        public LocatorProcess(IConnectionStringSource connectionStringSource, ILocatorConverter<SqlDataReader> converter) : base(connectionStringSource)
+        {
+            r_Converter = converter;
+        }
+    }
+}
