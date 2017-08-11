11bd7e1f9ada22962191f2c39a507e6c6a4ef6f8
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-11 19:25:34 +0800

    Added: IExportActualTimeLog.cs; IExportLocator.cs; Removed: IExportTimeLog.cs;

---
+++ LGU.Windows/Reports/HumanResource/[Contracts]/IExportActualTimeLog.cs
@@ -0,0 +1,11 @@
+﻿using LGU.Entities.HumanResource;
+using System.Collections.Generic;
+
+namespace LGU.Reports.HumanResource
+{
+    public interface IExportActualTimeLog : IExport
+    {
+        bool PrintAfterSave { get; set; }
+        IEnumerable<TimeLog> TimeLogs { get; set; }
+    }
+}
---
+++ LGU.Windows/Reports/HumanResource/[Contracts]/IExportLocator.cs
@@ -0,0 +1,9 @@
+﻿using LGU.Entities.HumanResource;
+
+namespace LGU.Reports.HumanResource
+{
+    public interface IExportLocator : IExport
+    {
+        Locator Locator { get; set; }
+    }
+}
