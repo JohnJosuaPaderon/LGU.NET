ba2fa6e5fc64df2f673142b730fbfbe35ea75507
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-11 19:38:31 +0800

    Added: AddEditLocatorDialogView.xaml; AddEditLocatorDialogView.xaml.cs;

---
+++ LGU.WindowsUI/Views/HumanResource/Dialogs/AddEditLocatorDialogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for AddEditLocatorDialogView
+    /// </summary>
+    public partial class AddEditLocatorDialogView : UserControl
+    {
+        public AddEditLocatorDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
