0406f66609dbf87f05e63ade4bdb8c3af8e674dc
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-11 19:23:42 +0800

    Added: App.config; App.xaml; App.xaml.cs; Bootstrapper.cs; LGU.HumanResource.Kiosk.csproj; logo.ico; packages.config;

---
+++ LGU.HumanResource.Kiosk/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using LGU.ViewModels;
+using LGU.Views.HumanResource;
+using System.Windows;
+
+namespace LGU.HumanResource.Kiosk
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindowViewModel.InitialMainContentRegionSource = nameof(MainKioskView);
+            new Bootstrapper().Run();
+        }
+    }
+}
---
+++ LGU.HumanResource.Kiosk/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using LGU.Extensions;
+using LGU.Views.HumanResource;
+using Prism.Unity;
+
+namespace LGU.HumanResource.Kiosk
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterTypeForNavigation<MainKioskView>();
+            Container.RegisterTypeForNavigation<KioskServiceSelectionView>();
+            Container.RegisterTypeForNavigation<KioskLocatorView>();
+        }
+
+        protected override void InitializeServices()
+        {
+            base.InitializeServices();
+            ServiceCollection.UseDigitalPersona();
+        }
+    }
+}
