c3957107f006fa3865d39faa49d32cbd21490324
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-11 19:28:27 +0800

    Added: DirectoryResolver.cs;

---
+++ LGU.Windows/Utilities/DirectoryResolver.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace LGU.Utilities
+{
+    public static class DirectoryResolver
+    {
+        public static void Resolve(string directory)
+        {
+            if (!string.IsNullOrWhiteSpace(directory))
+            {
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);
+                }
+            }
+        }
+    }
+}
