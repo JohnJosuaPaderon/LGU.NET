e4dba5394122732994a7deb70d33ad15db4e4868
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-11 19:20:46 +0800

    Added: ValueRange.cs; Updated: LGU.csproj; SystemRuntime.cs;

--- LGU/SystemRuntime.cs
+++ LGU/SystemRuntime.cs
@@ -14,14 +14,32 @@ namespace LGU
 
         public static IServiceProvider Services { get; private set; }
         public static string SystemDirectory { get; set; }
+        public static string ReportDirectory { get; set; }
+        public static string ReportTemplateDirectory { get; set; }
         public static User CurrentUser { get; set; }
+        public static bool DebugMode { get; set; }
 
         public static string ResolveSystemPath(string path)
+        {
+            return ResolvePath(SystemDirectory, path);
+        }
+
+        public static string ResolveReportPath(string path)
+        {
+            return ResolvePath(ReportDirectory, path);
+        }
+
+        public static string ResolveReportTemplatePath(string path)
+        {
+            return ResolvePath(ReportTemplateDirectory, path);
+        }
+
+        private static string ResolvePath(string baseDirectory, string path)
         {
             if (path.StartsWith("~"))
             {
                 path = path.Substring(2, path.Length - 2);
-                return Path.Combine(SystemDirectory, path);
+                return Path.Combine(baseDirectory, path);
             }
             else
             {
@@ -35,6 +53,11 @@ namespace LGU
             {
                 return CurrentUser?.DisplayName ?? "UNDEFINED";
             }
+        }
+
+        public static T GetService<T>()
+        {
+            return Services.GetService<T>();
         }
     }
 }
---
+++ LGU/ValueRange.cs
@@ -0,0 +1,20 @@
+﻿namespace LGU
+{
+    public struct ValueRange<T>
+    {
+        public ValueRange(T dateTime)
+        {
+            Begin = dateTime;
+            End = dateTime;
+        }
+
+        public ValueRange(T begin, T end)
+        {
+            Begin = begin;
+            End = end;
+        }
+
+        public T Begin { get; set; }
+        public T End { get; set; }
+    }
+}
