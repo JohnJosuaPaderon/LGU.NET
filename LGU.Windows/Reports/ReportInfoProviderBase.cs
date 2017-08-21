05b5b22d4fc4a7ff9252ae049be7eb42bf27a9ed
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-21 23:08:39 +0800

    Added: ReportInfoProviderBase.cs;

---
+++ LGU.Windows/Reports/ReportInfoProviderBase.cs
@@ -0,0 +1,36 @@
+﻿using LGU.Extensions;
+using System;
+using System.Configuration;
+
+namespace LGU.Reports
+{
+    public abstract class ReportInfoProviderBase
+    {
+        public ReportInfoProviderBase(string appSettingOwner)
+        {
+            if (string.IsNullOrWhiteSpace(appSettingOwner))
+            {
+                throw new ArgumentException("Application settings owner is invalid.", nameof(appSettingOwner));
+            }
+
+            AppSettingOwner = appSettingOwner;
+        }
+
+        private string AppSettingOwner { get; }
+
+        protected string GetReportTemplatePath(string key)
+        {
+            return SystemRuntime.ResolveReportTemplatePath(ConfigurationManager.AppSettings.GetString(AppendAppSettingOwner(key)));
+        }
+
+        protected string GetReportDirectory(string key)
+        {
+            return SystemRuntime.ResolveReportPath(ConfigurationManager.AppSettings.GetString(AppendAppSettingOwner(key)));
+        }
+
+        protected string AppendAppSettingOwner(string key)
+        {
+            return $"{AppSettingOwner}.{key}";
+        }
+    }
+}
