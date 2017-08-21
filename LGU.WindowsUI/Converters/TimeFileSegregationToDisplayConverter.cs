823a24a55fda300594465f81958f76c1ba50b7ca
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-21 23:09:30 +0800

    Added: TimeLogFileSegregationToDisplayConverter.cs; TimeLogExportOptionToDisplayConverter.cs;

---
+++ LGU.WindowsUI/Converters/TimeFileSegregationToDisplayConverter.cs
@@ -0,0 +1,42 @@
+﻿using LGU.Reports.HumanResource;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LGU.Converters
+{
+    public sealed class TimeFileSegregationToDisplayConverter : IValueConverter
+    {
+        public static TimeFileSegregationToDisplayConverter Instance { get; } = new TimeFileSegregationToDisplayConverter();
+
+        private const string ONE_FILE = "One File";
+        private const string PER_DEPARTMENT = "Per Department";
+        private const string PER_EMPLOYEE = "Per Employee";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fileSegregation = (TimeLogFileSegregation)value;
+
+            switch (fileSegregation)
+            {
+                case TimeLogFileSegregation.OneFile: return ONE_FILE;
+                case TimeLogFileSegregation.PerDepartment: return PER_DEPARTMENT;
+                case TimeLogFileSegregation.PerEmployee: return PER_EMPLOYEE;
+                default: return ONE_FILE;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var fileSegregation = (string)value;
+
+            switch (fileSegregation)
+            {
+                case ONE_FILE: return TimeLogFileSegregation.OneFile;
+                case PER_DEPARTMENT: return TimeLogFileSegregation.PerDepartment;
+                case PER_EMPLOYEE: return TimeLogFileSegregation.PerEmployee;
+                default: return TimeLogFileSegregation.OneFile;
+            }
+        }
+    }
+}
---
+++ LGU.WindowsUI/Converters/TimeLogExportOptionToDisplayConverter.cs
@@ -0,0 +1,42 @@
+﻿using LGU.Reports.HumanResource;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LGU.Converters
+{
+    public sealed class TimeLogExportOptionToDisplayConverter : IValueConverter
+    {
+        public static TimeLogExportOptionToDisplayConverter Instance { get; } = new TimeLogExportOptionToDisplayConverter();
+
+        private const string ALL = "All";
+        private const string SELECTED_DEPARTMENT = "Selected Department";
+        private const string SELECTED_EMPLOYEE = "Selected Employee";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var exportOption = (TimeLogExportOption)value;
+
+            switch (exportOption)
+            {
+                case TimeLogExportOption.All: return ALL;
+                case TimeLogExportOption.SelectedDepartment: return SELECTED_DEPARTMENT;
+                case TimeLogExportOption.SelectedEmployee: return SELECTED_EMPLOYEE;
+                default: return ALL;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var exportOption = (string)value;
+
+            switch (exportOption)
+            {
+                case ALL: return TimeLogExportOption.All;
+                case SELECTED_DEPARTMENT: return TimeLogExportOption.SelectedDepartment;
+                case SELECTED_EMPLOYEE: return TimeLogExportOption.SelectedEmployee;
+                default: return TimeLogExportOption.All;
+            }
+        }
+    }
+}
