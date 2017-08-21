234912f3d3f7848fcf18fc7cdf1026b7764272ff
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-21 23:05:57 +0800

    Added: TimeLogResolver.cs;

---
+++ LGU/Utilities/TimeLogResolver.cs
@@ -0,0 +1,28 @@
+﻿using LGU.Entities.HumanResource;
+using System;
+using System.Collections.Generic;
+
+namespace LGU.Utilities
+{
+    public class TimeLogResolver
+    {
+        public TimeLogResolver(IEnumerable<TimeLog> timeLogs)
+        {
+            TimeLogs = timeLogs ?? throw new ArgumentNullException(nameof(timeLogs));
+        }
+
+        public IEnumerable<TimeLog> TimeLogs { get; }
+
+        public IEnumerable<TimeLog> Resolve()
+        {
+            var timeLogs = new List<TimeLog>();
+
+            foreach (var rawTimeLog in TimeLogs)
+            {
+                
+            }
+
+            return timeLogs;
+        }
+    }
+}
