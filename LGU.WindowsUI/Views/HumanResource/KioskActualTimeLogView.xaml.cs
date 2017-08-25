8a15f30c0d6d32f0a1c362e4b8c77e7a081b11a9
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-25 08:21:37 +0800

    Added: KioskActualTimeLogView.xaml; KioskActualTimeLogView.xaml.cs; Updated: KioskLocatorView.xaml; KioskServiceSelectionView.xaml; MainKioskView.xaml; TimeLogExportView.xaml;

---
+++ LGU.WindowsUI/Views/HumanResource/KioskActualTimeLogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource
+{
+    /// <summary>
+    /// Interaction logic for KioskActualTimeLogView
+    /// </summary>
+    public partial class KioskActualTimeLogView : UserControl
+    {
+        public KioskActualTimeLogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
