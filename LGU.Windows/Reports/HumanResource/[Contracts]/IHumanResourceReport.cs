15ce719dc372c878818c004a02cb59aa89a1379e
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-25 08:17:12 +0800

    Added: IHumanResourceReport.cs;

---
+++ LGU.Windows/Reports/HumanResource/[Contracts]/IHumanResourceReport.cs
@@ -0,0 +1,15 @@
+﻿using LGU.Entities.HumanResource;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LGU.Reports.HumanResource
+{
+    public interface IHumanResourceReport
+    {
+        void ExportTimeLog(IEnumerable<TimeLog> timeLog, ValueRange<DateTime> cutOffs, TimeLogExportOption exportOption, TimeLogFileSegregation fileSegregation, IExportEventHandler eventHandler);
+        Task ExportTimeLogAsync(IEnumerable<TimeLog> timeLogs, ValueRange<DateTime> cutOff, TimeLogExportOption exportOption, TimeLogFileSegregation fileSegregation, IExportEventHandler eventHandler);
+        void ExportLocator(Locator locator, IExportEventHandler eventHandler);
+        Task ExportLocatorAsync(Locator locator, IExportEventHandler eventHandler);
+    }
+}
