5b169bff9ef5c73652d89e356e3fa18c8708f2eb
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-30 10:22:47 +0800

    Added: BooleanToVisibilityConverter.cs;

---
+++ LGU.WindowsUI/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LGU.Converters
+{
+    public sealed class BooleanToVisibilityConverter : IValueConverter
+    {
+        public static BooleanToVisibilityConverter Instance { get; } = new BooleanToVisibilityConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
