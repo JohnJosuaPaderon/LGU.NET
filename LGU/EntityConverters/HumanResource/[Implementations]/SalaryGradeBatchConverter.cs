8066d1485bedca3d0a642b9de52a91b442691cf5
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-30 10:17:51 +0800

    Added: SalaryGradeBatchConverter.cs; SalaryGradeConverter.cs;

---
+++ LGU/EntityConverters/HumanResource/[Implementations]/SalaryGradeBatchConverter.cs
@@ -0,0 +1,116 @@
+﻿using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityConverters.HumanResource
+{
+    public sealed class SalaryGradeBatchConverter : ISalaryGradeBatchConverter<SqlDataReader>
+    {
+        private ISalaryGradeBatch GetData(SqlDataReader reader)
+        {
+            return new SalaryGradeBatch()
+            {
+            };
+        }
+
+        public IEnumerableProcessResult<ISalaryGradeBatch> EnumerableFromReader(SqlDataReader reader)
+        {
+            try
+            {
+                var list = new List<ISalaryGradeBatch>();
+
+                while (reader.Read())
+                {
+                    list.Add(GetData(reader));
+                }
+
+                return new EnumerableProcessResult<ISalaryGradeBatch>(list);
+            }
+            catch (Exception ex)
+            {
+                return new EnumerableProcessResult<ISalaryGradeBatch>(ex);
+            }
+        }
+
+        public async Task<IEnumerableProcessResult<ISalaryGradeBatch>> EnumerableFromReaderAsync(SqlDataReader reader)
+        {
+            try
+            {
+                var list = new List<ISalaryGradeBatch>();
+
+                while (await reader.ReadAsync())
+                {
+                    list.Add(GetData(reader));
+                }
+
+                return new EnumerableProcessResult<ISalaryGradeBatch>(list);
+            }
+            catch (Exception ex)
+            {
+                return new EnumerableProcessResult<ISalaryGradeBatch>(ex);
+            }
+        }
+
+        public async Task<IEnumerableProcessResult<ISalaryGradeBatch>> EnumerableFromReaderAsync(SqlDataReader reader, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var list = new List<ISalaryGradeBatch>();
+
+                while (await reader.ReadAsync(cancellationToken))
+                {
+                    list.Add(GetData(reader));
+                }
+
+                return new EnumerableProcessResult<ISalaryGradeBatch>(list);
+            }
+            catch (Exception ex)
+            {
+                return new EnumerableProcessResult<ISalaryGradeBatch>(ex);
+            }
+        }
+
+        public IProcessResult<ISalaryGradeBatch> FromReader(SqlDataReader reader)
+        {
+            try
+            {
+                reader.Read();
+                return new ProcessResult<ISalaryGradeBatch>(GetData(reader));
+            }
+            catch (Exception ex)
+            {
+                return new ProcessResult<ISalaryGradeBatch>(ex);
+            }
+        }
+
+        public async Task<IProcessResult<ISalaryGradeBatch>> FromReaderAsync(SqlDataReader reader)
+        {
+            try
+            {
+                await reader.ReadAsync();
+                return new ProcessResult<ISalaryGradeBatch>(GetData(reader));
+            }
+            catch (Exception ex)
+            {
+                return new ProcessResult<ISalaryGradeBatch>(ex);
+            }
+        }
+
+        public async Task<IProcessResult<ISalaryGradeBatch>> FromReaderAsync(SqlDataReader reader, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await reader.ReadAsync(cancellationToken);
+                return new ProcessResult<ISalaryGradeBatch>(GetData(reader));
+            }
+            catch (Exception ex)
+            {
+                return new ProcessResult<ISalaryGradeBatch>(ex);
+            }
+        }
+    }
+}
---
+++ LGU/EntityConverters/HumanResource/[Implementations]/SalaryGradeConverter.cs
@@ -0,0 +1,146 @@
+﻿using LGU.Data.Extensions;
+using LGU.Entities.HumanResource;
+using LGU.EntityManagers.HumanResource;
+using LGU.Processes;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityConverters.HumanResource
+{
+    public sealed class SalaryGradeConverter : ISalaryGradeConverter<SqlDataReader>
+    {
+        private readonly ISalaryGradeBatchManager r_SalaryGradeBatchManager;
+
+        public SalaryGradeConverter(ISalaryGradeBatchManager salaryGradeBatchManager)
+        {
+            r_SalaryGradeBatchManager = salaryGradeBatchManager;
+        }
+
+        private ISalaryGrade GetData(ISalaryGradeBatch batch, SqlDataReader reader)
+        {
+            return new SalaryGrade(batch, reader.GetInt32("Number"))
+            {
+            };
+        }
+
+        private ISalaryGrade GetData(SqlDataReader reader)
+        {
+            var batchResult = r_SalaryGradeBatchManager.GetById(reader.GetInt32("BatchId"));
+
+            return GetData(batchResult.Data, reader);
+        }
+
+        private async Task<ISalaryGrade> GetDataAsync(SqlDataReader reader)
+        {
+            var batchResult = await r_SalaryGradeBatchManager.GetByIdAsync(reader.GetInt32("BatchId"));
+
+            return GetData(batchResult.Data, reader);
+        }
+
+        private async Task<ISalaryGrade> GetDataAsync(SqlDataReader reader, CancellationToken cancellationToken)
+        {
+            var batchResult = await r_SalaryGradeBatchManager.GetByIdAsync(reader.GetInt32("BatchId"), cancellationToken);
+
+            return GetData(batchResult.Data, reader);
+        }
+
+        public IEnumerableProcessResult<ISalaryGrade> EnumerableFromReader(SqlDataReader reader)
+        {
+            try
+            {
+                var list = new List<ISalaryGrade>();
+
+                while (reader.Read())
+                {
+                    list.Add(GetData(reader));
+                }
+
+                return new EnumerableProcessResult<ISalaryGrade>(list);
+            }
+            catch (Exception ex)
+            {
+                return new EnumerableProcessResult<ISalaryGrade>(ex);
+            }
+        }
+
+        public async Task<IEnumerableProcessResult<ISalaryGrade>> EnumerableFromReaderAsync(SqlDataReader reader)
+        {
+            try
+            {
+                var list = new List<ISalaryGrade>();
+
+                while (await reader.ReadAsync())
+                {
+                    list.Add(GetData(reader));
+                }
+
+                return new EnumerableProcessResult<ISalaryGrade>(list);
+            }
+            catch (Exception ex)
+            {
+                return new EnumerableProcessResult<ISalaryGrade>(ex);
+            }
+        }
+
+        public async Task<IEnumerableProcessResult<ISalaryGrade>> EnumerableFromReaderAsync(SqlDataReader reader, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var list = new List<ISalaryGrade>();
+
+                while (await reader.ReadAsync(cancellationToken))
+                {
+                    list.Add(GetData(reader));
+                }
+
+                return new EnumerableProcessResult<ISalaryGrade>(list);
+            }
+            catch (Exception ex)
+            {
+                return new EnumerableProcessResult<ISalaryGrade>(ex);
+            }
+        }
+
+        public IProcessResult<ISalaryGrade> FromReader(SqlDataReader reader)
+        {
+            try
+            {
+                reader.Read();
+                return new ProcessResult<ISalaryGrade>(GetData(reader));
+            }
+            catch (Exception ex)
+            {
+                return new ProcessResult<ISalaryGrade>(ex);
+            }
+        }
+
+        public async Task<IProcessResult<ISalaryGrade>> FromReaderAsync(SqlDataReader reader)
+        {
+            try
+            {
+                await reader.ReadAsync();
+                return new ProcessResult<ISalaryGrade>(GetData(reader));
+            }
+            catch (Exception ex)
+            {
+                return new ProcessResult<ISalaryGrade>(ex);
+            }
+        }
+
+        public async Task<IProcessResult<ISalaryGrade>> FromReaderAsync(SqlDataReader reader, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await reader.ReadAsync(cancellationToken);
+                return new ProcessResult<ISalaryGrade>(GetData(reader));
+            }
+            catch (Exception ex)
+            {
+                return new ProcessResult<ISalaryGrade>(ex);
+            }
+        }
+    }
+}
