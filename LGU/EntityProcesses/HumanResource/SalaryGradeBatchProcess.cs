8c756f527bc98a5858b7c697ca53125bf3e4dabb
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-30 10:21:51 +0800

    Added: SalaryGradeBatchProcess.cs; SalaryGradeProcess.cs;

---
+++ LGU/EntityProcesses/HumanResource/SalaryGradeBatchProcess.cs
@@ -0,0 +1,15 @@
+﻿using LGU.EntityConverters.HumanResource;
+using System.Data.SqlClient;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public abstract class SalaryGradeBatchProcess : HumanResourceProcessBase
+    {
+        protected readonly ISalaryGradeBatchConverter<SqlDataReader> r_Converter;
+
+        public SalaryGradeBatchProcess(IConnectionStringSource connectionStringSource, ISalaryGradeBatchConverter<SqlDataReader> converter) : base(connectionStringSource)
+        {
+            r_Converter = converter;
+        }
+    }
+}
---
+++ LGU/EntityProcesses/HumanResource/SalaryGradeProcess.cs
@@ -0,0 +1,15 @@
+﻿using LGU.EntityConverters.HumanResource;
+using System.Data.SqlClient;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public abstract class SalaryGradeProcess : HumanResourceProcessBase
+    {
+        protected readonly ISalaryGradeConverter<SqlDataReader> r_Converter;
+
+        public SalaryGradeProcess(IConnectionStringSource connectionStringSource, ISalaryGradeConverter<SqlDataReader> converter) : base(connectionStringSource)
+        {
+            r_Converter = converter;
+        }
+    }
+}
