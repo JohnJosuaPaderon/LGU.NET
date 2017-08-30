1e6073f91cbf477b45423e21d40b3d0aa0ae9cc0
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-30 10:25:28 +0800

    Added: SalaryGradeManagementView.xaml; SalaryGradeManagementView.xaml.cs; Updated: KioskActualTimeLogView.xaml; LauncherView.xaml; MainKioskView.xaml; PreviewEmployeeView.xaml;

---
+++ LGU.WindowsUI/Views/HumanResource/SalaryGradeManagementView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource
+{
+    /// <summary>
+    /// Interaction logic for SalaryGradeManagementView
+    /// </summary>
+    public partial class SalaryGradeManagementView : UserControl
+    {
+        public SalaryGradeManagementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
