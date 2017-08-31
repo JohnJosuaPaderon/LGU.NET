65768350d8e2e7f0fbfe279c001bd88bd058b25a
johnpad53 <johnjosua.paderon@outlook.com>
2017-08-31 22:37:19 +0800

    Updated: ISalaryGradeManager.cs; ISalaryGradeStepManager.cs;

--- LGU/EntityManagers/HumanResource/[Contracts]/ISalaryGradeManager.cs
+++ LGU/EntityManagers/HumanResource/[Contracts]/ISalaryGradeManager.cs
@@ -1,9 +1,15 @@
 ﻿using LGU.Entities;
 using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LGU.EntityManagers.HumanResource
 {
     public interface ISalaryGradeManager : IEntityManager<ISalaryGrade, long>
     {
+        IEnumerableProcessResult<ISalaryGrade> GetListByBatch(ISalaryGradeBatch batch);
+        Task<IEnumerableProcessResult<ISalaryGrade>> GetListByBatchAsync(ISalaryGradeBatch batch);
+        Task<IEnumerableProcessResult<ISalaryGrade>> GetListByBatchAsync(ISalaryGradeBatch batch, CancellationToken cancellationToken);
     }
 }
--- LGU/EntityManagers/HumanResource/[Contracts]/ISalaryGradeStepManager.cs
+++ LGU/EntityManagers/HumanResource/[Contracts]/ISalaryGradeStepManager.cs
@@ -1,9 +1,15 @@
 ﻿using LGU.Entities;
 using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LGU.EntityManagers.HumanResource
 {
-    public interface ISalaryGradeStepManager : IEntityManager<SalaryGradeStep, long>
+    public interface ISalaryGradeStepManager : IEntityManager<ISalaryGradeStep, long>
     {
+        IEnumerableProcessResult<ISalaryGradeStep> GetListBySalaryGrade(ISalaryGrade salaryGrade);
+        Task<IEnumerableProcessResult<ISalaryGradeStep>> GetListBySalaryGradeAsync(ISalaryGrade salaryGrade);
+        Task<IEnumerableProcessResult<ISalaryGradeStep>> GetListBySalaryGradeAsync(ISalaryGrade salaryGrade, CancellationToken cancellationToken);
     }
 }
