012a5b27688628407b23a44d9ebc47694f9e134a
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-01 21:45:34 +0800

    Added: GetCurrentSalaryGradeBatch.cs;

---
+++ LGU/EntityProcesses/HumanResource/[Implementations]/GetCurrentSalaryGradeBatch.cs
@@ -0,0 +1,34 @@
+﻿using LGU.Data.Rdbms;
+using LGU.Entities.HumanResource;
+using LGU.EntityConverters.HumanResource;
+using LGU.Processes;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public sealed class GetCurrentSalaryGradeBatch : SalaryGradeBatchProcess, IGetCurrentSalaryGradeBatch
+    {
+        public GetCurrentSalaryGradeBatch(IConnectionStringSource connectionStringSource, ISalaryGradeBatchConverter<SqlDataReader> converter) : base(connectionStringSource, converter)
+        {
+        }
+
+        private SqlQueryInfo QueryInfo => SqlQueryInfo.CreateProcedureQueryInfo(GetQualifiedDbObjectName());
+
+        public IProcessResult<ISalaryGradeBatch> Execute()
+        {
+            return r_SqlHelper.ExecuteReader(QueryInfo, r_Converter);
+        }
+
+        public Task<IProcessResult<ISalaryGradeBatch>> ExecuteAsync()
+        {
+            return r_SqlHelper.ExecuteReaderAsync(QueryInfo, r_Converter);
+        }
+
+        public Task<IProcessResult<ISalaryGradeBatch>> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return r_SqlHelper.ExecuteReaderAsync(QueryInfo, r_Converter, cancellationToken);
+        }
+    }
+}
