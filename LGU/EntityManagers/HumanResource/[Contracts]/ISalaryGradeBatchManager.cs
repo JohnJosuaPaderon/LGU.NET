e653d39ab34dfe5a94bfef9be2f62b1b27170de5
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-01 21:44:50 +0800

    Updated: ISalaryGradeBatchManager.cs;

--- LGU/EntityManagers/HumanResource/[Contracts]/ISalaryGradeBatchManager.cs
+++ LGU/EntityManagers/HumanResource/[Contracts]/ISalaryGradeBatchManager.cs
@@ -1,9 +1,15 @@
 ﻿using LGU.Entities;
 using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace LGU.EntityManagers.HumanResource
 {
     public interface ISalaryGradeBatchManager : IEntityManager<ISalaryGradeBatch, int>
     {
+        IProcessResult<ISalaryGradeBatch> GetCurrent();
+        Task<IProcessResult<ISalaryGradeBatch>> GetCurrentAsync();
+        Task<IProcessResult<ISalaryGradeBatch>> GetCurrentAsync(CancellationToken cancellationToken);
     }
 }
