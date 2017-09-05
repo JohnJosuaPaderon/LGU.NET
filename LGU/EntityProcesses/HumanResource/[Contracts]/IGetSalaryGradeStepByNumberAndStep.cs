c8ef3442fea01b6cd032f3eba694b9e16cdbd104
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-05 10:06:44 +0800

    Added: IDeleteEmployeeSalaryGradeStep.cs; IGetEmployeeSalaryGradeStepList.cs; IGetSalaryGradeSepByNumberAndStep.cs;

---
+++ LGU/EntityProcesses/HumanResource/[Contracts]/IGetSalaryGradeStepByNumberAndStep.cs
@@ -0,0 +1,11 @@
+﻿using LGU.Entities.HumanResource;
+using LGU.Processes;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public interface IGetSalaryGradeStepByNumberAndStep : IProcess<ISalaryGradeStep>
+    {
+        int SalaryGradeNumber { get; set; }
+        int Step { get; set; }
+    }
+}
---
+++ LGU/EntityProcesses/HumanResource/[Contracts]/IInsertEmployeeSalaryGradeStep.cs
@@ -0,0 +1,10 @@
+﻿using LGU.Entities.HumanResource;
+using LGU.Processes;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public interface IInsertEmployeeSalaryGradeStep : IProcess<IEmployeeSalaryGradeStep>
+    {
+        IEmployeeSalaryGradeStep EmployeeSalaryGradeStep { get; set; }
+    }
+}
