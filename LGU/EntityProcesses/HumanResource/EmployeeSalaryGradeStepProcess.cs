42431cb951cfc17dd9e69cd5be634ea919c2fde2
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-05 10:08:11 +0800

    Added: EmployeeSalaryGradeStepProcess.cs;

---
+++ LGU/EntityProcesses/HumanResource/EmployeeSalaryGradeStepProcess.cs
@@ -0,0 +1,15 @@
+﻿using LGU.EntityConverters.HumanResource;
+using System.Data.SqlClient;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public abstract class EmployeeSalaryGradeStepProcess : HumanResourceProcessBase
+    {
+        public EmployeeSalaryGradeStepProcess(IConnectionStringSource connectionStringSource, IEmployeeSalaryGradeStepConverter<SqlDataReader> converter) : base(connectionStringSource)
+        {
+            r_Converter = converter;
+        }
+
+        protected readonly IEmployeeSalaryGradeStepConverter<SqlDataReader> r_Converter;
+    }
+}
