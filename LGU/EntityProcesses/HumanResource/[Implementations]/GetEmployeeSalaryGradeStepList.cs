842a7a9cceb452a3cbcff5c605d2cfd7c17aa8aa
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-05 10:07:37 +0800

    Added: DeleteEmployeeSalaryGradeStep.cs; GetEmployeeSalaryGradeStepList.cs; GetSalaryGradeStepByNumberAndStep.cs; InsertEmployeeSalaryGradeStep.cs; UpdateEmployeeSalaryGradeStep.cs;

---
+++ LGU/EntityProcesses/HumanResource/[Implementations]/GetEmployeeSalaryGradeStepList.cs
@@ -0,0 +1,34 @@
+﻿using LGU.Data.Rdbms;
+using LGU.Entities.HumanResource;
+using LGU.EntityConverters.HumanResource;
+using LGU.Processes;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public sealed class GetEmployeeSalaryGradeStepList : EmployeeSalaryGradeStepProcess, IGetEmployeeSalaryGradeStepList
+    {
+        public GetEmployeeSalaryGradeStepList(IConnectionStringSource connectionStringSource, IEmployeeSalaryGradeStepConverter<SqlDataReader> converter) : base(connectionStringSource, converter)
+        {
+        }
+
+        private SqlQueryInfo QueryInfo => SqlQueryInfo.CreateProcedureQueryInfo(GetQualifiedDbObjectName());
+
+        public IEnumerableProcessResult<IEmployeeSalaryGradeStep> Execute()
+        {
+            return r_SqlHelper.ExecuteReaderEnumerable(QueryInfo, r_Converter);
+        }
+
+        public Task<IEnumerableProcessResult<IEmployeeSalaryGradeStep>> ExecuteAsync()
+        {
+            return r_SqlHelper.ExecuteReaderEnumerableAsync(QueryInfo, r_Converter);
+        }
+
+        public Task<IEnumerableProcessResult<IEmployeeSalaryGradeStep>> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return r_SqlHelper.ExecuteReaderEnumerableAsync(QueryInfo, r_Converter, cancellationToken);
+        }
+    }
+}
---
+++ LGU/EntityProcesses/HumanResource/[Implementations]/InsertEmployeeSalaryGradeStep.cs
@@ -0,0 +1,54 @@
+﻿using LGU.Data.Extensions;
+using LGU.Data.Rdbms;
+using LGU.Entities.HumanResource;
+using LGU.EntityConverters.HumanResource;
+using LGU.Processes;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public sealed class InsertEmployeeSalaryGradeStep : EmployeeSalaryGradeStepProcess, IInsertEmployeeSalaryGradeStep
+    {
+        public InsertEmployeeSalaryGradeStep(IConnectionStringSource connectionStringSource, IEmployeeSalaryGradeStepConverter<SqlDataReader> converter) : base(connectionStringSource, converter)
+        {
+        }
+
+        public IEmployeeSalaryGradeStep EmployeeSalaryGradeStep { get; set; }
+
+        private SqlQueryInfo<IEmployeeSalaryGradeStep> QueryInfo =>
+            SqlQueryInfo<IEmployeeSalaryGradeStep>.CreateProcedureQueryInfo(EmployeeSalaryGradeStep, GetQualifiedDbObjectName(), GetProcessResult, true)
+            .AddInputParameter("@_EmployeeId", EmployeeSalaryGradeStep.Employee?.Id)
+            .AddInputParameter("@_SalaryGradeStepId", EmployeeSalaryGradeStep.SalaryGradeStep?.Id)
+            .AddInputParameter("@_EffectivityDate", EmployeeSalaryGradeStep.EffectivityDate)
+            .AddLogByParameter();
+
+        private IProcessResult<IEmployeeSalaryGradeStep> GetProcessResult(IEmployeeSalaryGradeStep data, SqlCommand command, int affectedRows)
+        {
+            if (affectedRows > 0)
+            {
+                return new ProcessResult<IEmployeeSalaryGradeStep>(data);
+            }
+            else
+            {
+                return new ProcessResult<IEmployeeSalaryGradeStep>(ProcessResultStatus.Failed, "Failed to insert employee salary grade step.");
+            }
+        }
+
+        public IProcessResult<IEmployeeSalaryGradeStep> Execute()
+        {
+            return r_SqlHelper.ExecuteNonQuery(QueryInfo);
+        }
+
+        public Task<IProcessResult<IEmployeeSalaryGradeStep>> ExecuteAsync()
+        {
+            return r_SqlHelper.ExecuteNonQueryAsync(QueryInfo);
+        }
+
+        public Task<IProcessResult<IEmployeeSalaryGradeStep>> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return r_SqlHelper.ExecuteNonQueryAsync(QueryInfo, cancellationToken);
+        }
+    }
+}
