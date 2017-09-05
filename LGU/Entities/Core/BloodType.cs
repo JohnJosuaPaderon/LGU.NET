2ffff0f5de1e2d86bd74ed627993ab6fb41399e6
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-05 09:56:13 +0800

    Added: BloodType.cs; PersonAddressType.cs;

---
+++ LGU/Entities/Core/BloodType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LGU.Entities.Core
+{
+    [Flags]
+    public enum BloodType
+    {
+        Unknown = 0,
+        A = 1,
+        B = 2,
+        O = 4
+    }
+}
---
+++ LGU/Entities/Core/PersonAddressType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LGU.Entities.Core
+{
+    [Flags]
+    public enum PersonAddressType
+    {
+        Residential = 0,
+        Permanent = 1
+    }
+}
