7006067dc3a52c285f672bc48461320b4bc32a1e
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-05 09:56:39 +0800

    Added: IEmployeeSalaryGradeStep.cs;

---
+++ LGU/Entities/HumanResource/[Contracts]/IEmployeeSalaryGradeStep.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LGU.Entities.HumanResource
+{
+    public interface IEmployeeSalaryGradeStep
+    {
+        IEmployee Employee { get; set; }
+        ISalaryGradeStep SalaryGradeStep { get; set; }
+        DateTime EffectivityDate { get; set; }
+    }
+}
