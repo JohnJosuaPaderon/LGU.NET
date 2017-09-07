56aa37f88bead5b0c7edf82142c47f9945d1f2a0
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-07 09:46:44 +0800

    Added: IWorkTimeScheduleConverter.cs;

---
+++ LGU/EntityConverters/HumanResource/[Contracts]/IWorkTimeScheduleConverter.cs
@@ -0,0 +1,11 @@
+﻿using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System.Data.Common;
+
+namespace LGU.EntityConverters.HumanResource
+{
+    public interface IWorkTimeScheduleConverter<TDataReader> : IDataConverter<IWorkTimeSchedule, TDataReader>
+        where TDataReader : DbDataReader
+    {
+    }
+}
