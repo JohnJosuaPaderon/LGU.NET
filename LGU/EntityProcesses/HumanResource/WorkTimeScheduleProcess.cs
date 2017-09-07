79cb050368b6e82c6dc9e0378a1c29995514e8bd
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-07 09:53:22 +0800

    Added: WorkTImeScheduleProcess.cs;

---
+++ LGU/EntityProcesses/HumanResource/WorkTimeScheduleProcess.cs
@@ -0,0 +1,15 @@
+﻿using LGU.EntityConverters.HumanResource;
+using System.Data.SqlClient;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public abstract class WorkTimeScheduleProcess : HumanResourceProcessBase
+    {
+        public WorkTimeScheduleProcess(IConnectionStringSource connectionStringSource, IWorkTimeScheduleConverter<SqlDataReader> converter) : base(connectionStringSource)
+        {
+            r_Converter = converter;
+        }
+
+        protected readonly IWorkTimeScheduleConverter<SqlDataReader> r_Converter;
+    }
+}
