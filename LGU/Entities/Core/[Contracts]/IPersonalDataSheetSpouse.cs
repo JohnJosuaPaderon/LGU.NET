8e5d73285fe6296d628d68c19281450dd4e06945
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-12 17:05:23 +0800

    Added: IPersonalDataSheetSpouse.cs;

---
+++ LGU/Entities/Core/[Contracts]/IPersonalDataSheetSpouse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LGU.Entities.Core
+{
+    public interface IPersonalDataSheetSpouse : IEntity<long>
+    {
+        string FirstName { get; set; }
+        string MiddleName { get; set; }
+        string LastName { get; set; }
+        string NameExtension { get; set; }
+    }
+}
