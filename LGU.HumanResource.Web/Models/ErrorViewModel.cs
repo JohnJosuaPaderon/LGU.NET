c722bb206fc0c350ea3ca85b2181ef43242d3c66
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-13 17:00:01 +0800

    Removed: LGU.HumanResource.Web Project.

--- LGU.HumanResource.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LGU.HumanResource.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- LGU.HumanResource.Web/Models/MenuItemModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LGU.HumanResource.Web.Models
-{
-    public class MenuItemModel
-    {
-        public string Description { get; set; }
-        public string Action { get; set; }
-        public string Controller { get; set; }
-    }
-}
