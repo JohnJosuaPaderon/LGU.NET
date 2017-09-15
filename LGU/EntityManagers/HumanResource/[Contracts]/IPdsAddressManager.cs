a5f0a246f34da777a1bc8def61f9885a0ffb1998
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-15 08:45:35 +0800

    Added: PdsAddressManager.cs;

---
+++ LGU/EntityManagers/HumanResource/[Contracts]/IPdsAddressManager.cs
@@ -0,0 +1,11 @@
+﻿using LGU.Entities;
+using LGU.Entities.HumanResource;
+using LGU.Processes;
+
+namespace LGU.EntityManagers.HumanResource
+{
+    public interface IPdsAddressManager : IEntityManager<IPdsAddress, long>
+    {
+        IProcessResult<string> ConstructFullName(IPdsAddress pdsAddress);
+    }
+}
