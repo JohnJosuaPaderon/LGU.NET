6947af30ebb9e681364a69c3936cb0f194c58eb1
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-15 08:47:45 +0800

    Added: PersonalDataSheetViewModel.cs;

---
+++ LGU.WindowsUI/ViewModels/HumanResource/PersonalDataSheetViewModel.cs
@@ -0,0 +1,37 @@
+﻿using LGU.Models.HumanResource;
+using Prism.Events;
+using Prism.Regions;
+
+namespace LGU.ViewModels.HumanResource
+{
+    public class PersonalDataSheetViewModel : ViewModelBase, INavigationAware
+    {
+        public PersonalDataSheetViewModel(IRegionManager regionManager, IEventAggregator eventAggregator) : base(regionManager, eventAggregator)
+        {
+        }
+
+        public EmployeeModel CurrentEmployee { get; set; }
+
+        protected override void Initialize()
+        {
+            r_ChangeHeaderEvent.Publish("Personal Data Sheet");
+        }
+
+        #region INavigationAware
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            return true;
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+
+        }
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            CurrentEmployee = navigationContext.Parameters["employee"] as EmployeeModel;
+        } 
+        #endregion
+    }
+}
