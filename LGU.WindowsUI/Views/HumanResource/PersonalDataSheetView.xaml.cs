80fabe2f3b0fcf7934b80af932e5303456ef41a7
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-15 08:48:27 +0800

    Added: PersonalDataSheetView.xaml; PersonalDataSheetView.xaml.cs;

---
+++ LGU.WindowsUI/Views/HumanResource/PersonalDataSheetView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource
+{
+    /// <summary>
+    /// Interaction logic for PersonalDataSheetView
+    /// </summary>
+    public partial class PersonalDataSheetView : UserControl
+    {
+        public PersonalDataSheetView()
+        {
+            InitializeComponent();
+        }
+    }
+}
