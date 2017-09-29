03de0cdbb6e304b5753bc4d976e9bf8b6d8b386f
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-29 11:29:53 +0800

    Added: DataConverterProperty.cs; IDataConverterProperty.cs;

---
+++ LGU/Processes/DataConverterProperty.cs
@@ -0,0 +1,24 @@
+﻿namespace LGU.Processes
+{
+    public class DataConverterProperty<T> : IDataConverterProperty<T>
+    {
+        private T _Value;
+
+        public bool UseProvidedValue { get; set; }
+        
+        public T Value
+        {
+            get { return _Value; }
+            set
+            {
+                UseProvidedValue = false;
+                _Value = value;
+            }
+        }
+
+        public T TryGetValue(T alternativeValue)
+        {
+            return UseProvidedValue ? Value : alternativeValue;
+        }
+    }
+}
---
+++ LGU/Processes/IDataConverterProperty.cs
@@ -0,0 +1,9 @@
+﻿namespace LGU.Processes
+{
+    public interface IDataConverterProperty<T>
+    {
+        bool UseProvidedValue { get; set; }
+        T Value { get; set; }
+        T TryGetValue(T alternativeValue);
+    }
+}
