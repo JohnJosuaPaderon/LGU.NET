c08a742fcad09ce1a8e9c57364b5e05456df1c67
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-29 23:42:03 +0800

    Updated: IProcess.cs;

--- LGU/Processes/IProcess.cs
+++ LGU/Processes/IProcess.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System.Data.Common;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LGU.Processes
@@ -15,5 +16,23 @@ namespace LGU.Processes
         IProcessResult<T> Execute();
         Task<IProcessResult<T>> ExecuteAsync();
         Task<IProcessResult<T>> ExecuteAsync(CancellationToken cancellationToken);
+    }
+
+    public interface IProcess<TConnection, TTransaction>
+        where TConnection : DbConnection
+        where TTransaction : DbTransaction
+    {
+        IProcessResult Execute(TConnection connection, TTransaction transaction);
+        Task<IProcessResult> ExecuteAsync(TConnection connection, TTransaction transaction);
+        Task<IProcessResult> ExecuteAsync(TConnection connection, TTransaction transaction, CancellationToken cancellationToken);
+    }
+
+    public interface IProcess<T, TConnection, TTransaction>
+        where TConnection : DbConnection
+        where TTransaction : DbTransaction
+    {
+        IProcessResult<T> Execute(TConnection connection, TTransaction transaction);
+        Task<IProcessResult<T>> ExecuteAsync(TConnection connection, TTransaction transaction);
+        Task<IProcessResult<T>> ExecuteAsync(TConnection connection, TTransaction transaction, CancellationToken cancellationToken);
     }
 }
