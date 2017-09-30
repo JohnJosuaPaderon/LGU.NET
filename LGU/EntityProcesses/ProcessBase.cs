0ae99c5ed1536d00caed3dfa13bd8264aafcf63d
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-30 10:51:38 +0800

    Updated: ProcessBase.cs;

--- LGU/EntityProcesses/ProcessBase.cs
+++ LGU/EntityProcesses/ProcessBase.cs
@@ -5,33 +5,11 @@ namespace LGU.EntityProcesses
     public abstract class ProcessBase
     {
         protected readonly SqlHelper _SqlHelper;
-        protected readonly string r_SchemaName;
+        protected readonly string _SchemaName;
 
         public ProcessBase(IConnectionStringSource connectionStringSource, string schemaName)
         {
             _SqlHelper = new SqlHelper(new SqlConnectionEstablisher(connectionStringSource[schemaName]));
-            r_SchemaName = string.IsNullOrWhiteSpace(schemaName) ? "Core" : schemaName;
-        }
-
-        protected string GetQualifiedDbObjectName(string dbObjectName)
-        {
-            return $"{r_SchemaName}.{dbObjectName}";
-        }
-
-        protected string GetQualifiedDbObjectName()
-        {
-            return GetQualifiedDbObjectName(GetType().Name);
-        }
-    }
-
-    public abstract class ProcessBaseV2
-    {
-        protected readonly SqlConnectionEstablisher _ConnectionEstablisher;
-        protected readonly string _SchemaName;
-
-        public ProcessBaseV2(IConnectionStringSource connectionStringSource, string schemaName)
-        {
-            _ConnectionEstablisher = new SqlConnectionEstablisher(connectionStringSource[schemaName]);
             _SchemaName = string.IsNullOrWhiteSpace(schemaName) ? "Core" : schemaName;
         }
 
@@ -43,6 +21,16 @@ namespace LGU.EntityProcesses
         protected string GetQualifiedDbObjectName()
         {
             return GetQualifiedDbObjectName(GetType().Name);
+        }
+    }
+
+    public abstract class ProcessBaseV2 : ProcessBase
+    {
+        protected readonly SqlConnectionEstablisher _ConnectionEstablisher;
+
+        public ProcessBaseV2(IConnectionStringSource connectionStringSource, string schemaName) : base(connectionStringSource, schemaName)
+        {
+            _ConnectionEstablisher = new SqlConnectionEstablisher(connectionStringSource[schemaName]);
         }
     }
 }
