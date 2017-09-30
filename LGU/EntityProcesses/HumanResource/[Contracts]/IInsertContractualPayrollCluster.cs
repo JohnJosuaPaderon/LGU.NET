9909e91cb194ce68301d496819cb774d13e02980
johnpad53 <johnjosua.paderon@outlook.com>
2017-09-30 10:51:08 +0800

    Updated: IInsertContractualPayrollCluster.cs;

--- LGU/EntityProcesses/HumanResource/[Contracts]/IInsertContractualPayrollCluster.cs
+++ LGU/EntityProcesses/HumanResource/[Contracts]/IInsertContractualPayrollCluster.cs
@@ -1,12 +1,10 @@
 ﻿using LGU.Entities.HumanResource;
 using LGU.Processes;
-using System.Data.Common;
 
 namespace LGU.EntityProcesses.HumanResource
 {
-    public interface IInsertContractualPayrollCluster<TConnection, TTransaction> : IProcess<IContractualPayrollCluster>, IProcess<IContractualPayrollCluster, TConnection, TTransaction>
-        where TConnection : DbConnection
-        where TTransaction : DbTransaction
+    public interface IInsertContractualPayrollCluster : IProcess<IContractualPayrollCluster>
     {
+        IContractualPayrollCluster PayrollCluster { get; set; }
     }
 }
