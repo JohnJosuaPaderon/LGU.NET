917f845374492756556c7e50ba5e8b34334013fc
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-04 10:59:00 +0800

    Added: ObservableCollectionExtension.cs;

---
+++ LGU.WindowsUI/Extensions/ObservableCollectionExtension.cs
@@ -0,0 +1,22 @@
+﻿using LGU.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace LGU.Extensions
+{
+    public static class ObservableCollectionExtension
+    {
+        public static IEnumerable<T> GetSource<T, TModel>(this ObservableCollection<TModel> instance)
+            where TModel : ModelBase<T>
+        {
+            var list = new List<T>();
+
+            foreach (var item in instance)
+            {
+                list.Add(item.GetSource());
+            }
+
+            return list;
+        }
+    }
+}
