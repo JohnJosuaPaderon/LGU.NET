a4eb62ace287d381344897003baaa7ad0a40253d
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-04 10:59:50 +0800

    Added: CustomPopupWindowAction.cs;

---
+++ LGU.WindowsUI/Interactivity/CustomPopupWindowAction.cs
@@ -0,0 +1,26 @@
+﻿using MahApps.Metro.Controls;
+using Prism.Interactivity;
+using System.Windows;
+using System.Windows.Media;
+
+namespace LGU.Interactivity
+{
+    public class CustomPopupWindowAction : PopupWindowAction
+    {
+        protected override Window CreateWindow()
+        {
+            return new MetroWindow()
+            {
+                SizeToContent = SizeToContent.WidthAndHeight,
+                ShowTitleBar = false,
+                ShowMinButton = false,
+                ShowMaxRestoreButton = false,
+                ShowCloseButton = false,
+                WindowStartupLocation = WindowStartupLocation ?? System.Windows.WindowStartupLocation.CenterScreen,
+                BorderBrush = new SolidColorBrush(Color.FromArgb(50, 0, 0, 0)),
+                BorderThickness = new Thickness(1),
+                Padding = new Thickness(50)
+            };
+        }
+    }
+}
