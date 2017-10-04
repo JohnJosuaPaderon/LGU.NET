86363b310157b2a191b02f41d35cb0d653209987
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-04 10:59:26 +0800

    Added: DateTimeUtilities.cs;

---
+++ LGU/Utilities/DateTimeUtilities.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LGU.Utilities
+{
+    public static class DateTimeUtilities
+    {
+        public static double GetTotalDayMinuteDiff(DateTime left, DateTime right)
+        {
+			return (left.TimeOfDay - right.TimeOfDay).TotalMinutes;
+        }
+    }
+}
