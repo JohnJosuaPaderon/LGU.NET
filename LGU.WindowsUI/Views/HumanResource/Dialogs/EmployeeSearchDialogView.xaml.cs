839c3682ec218708af720dffb96052f81ddaf28b
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-05 19:45:29 +0800

    Added: EmployeeSearchDialogView.xaml; EmployeeSearchDialogView.xaml.cs;
    Updated: AddEditEmployeeDialogView.xaml;

---
+++ LGU.WindowsUI/Views/HumanResource/Dialogs/EmployeeSearchDialogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for EmployeeSearchDialogView
+    /// </summary>
+    public partial class EmployeeSearchDialogView : UserControl
+    {
+        public EmployeeSearchDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
