f9de8060cd324f55bc5cc42d6556e20c095dfd7e
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-05 19:46:15 +0800

    Added: ContractualPayrollView.xaml; ContractualPayrollView.xaml.cs;
    Updated: KioskActualTimeLogView.xaml; PayrollStatupView.xaml;

---
+++ LGU.WindowsUI/Views/HumanResource/ContractualPayrollView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource
+{
+    /// <summary>
+    /// Interaction logic for ContractualPayrollView
+    /// </summary>
+    public partial class ContractualPayrollView : UserControl
+    {
+        public ContractualPayrollView()
+        {
+            InitializeComponent();
+        }
+    }
+}
