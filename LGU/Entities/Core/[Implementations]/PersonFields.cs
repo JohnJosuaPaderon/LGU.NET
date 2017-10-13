86fa779ddef33106da7a845035ae51e8ce641b68
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-13 15:26:26 +0800

    Added: PersonFields.cs; PersonParameters.cs;

---
+++ LGU/Entities/Core/[Implementations]/PersonFields.cs
@@ -0,0 +1,26 @@
+﻿namespace LGU.Entities.Core
+{
+    public class PersonFields : IPersonFields
+    {
+        public PersonFields()
+        {
+            Id = "Id";
+            FirstName = "FirstName";
+            MiddleName = "MiddleName";
+            LastName = "LastName";
+            NameExtension = "NameExtension";
+            BirthDate = "BirthDate";
+            Deceased = "Deceased";
+            GenderId = "GenderId";
+        }
+
+        public string Id { get; }
+        public string FirstName { get; }
+        public string MiddleName { get; }
+        public string LastName { get; }
+        public string NameExtension { get; }
+        public string BirthDate { get; }
+        public string Deceased { get; }
+        public string GenderId { get; }
+    }
+}
---
+++ LGU/Entities/Core/[Implementations]/PersonParameters.cs
@@ -0,0 +1,26 @@
+﻿namespace LGU.Entities.Core
+{
+    public class PersonParameters : IPersonParameters
+    {
+        public PersonParameters()
+        {
+            Id = "@_Id";
+            FirstName = "@_FirstName";
+            MiddleName = "@_MiddleName";
+            LastName = "@_LastName";
+            NameExtension = "@_NameExtension";
+            BirthDate = "@_BirthDate";
+            Deceased = "@_Deceased";
+            GenderId = "@_GenderId";
+        }
+
+        public string Id { get; }
+        public string FirstName { get; }
+        public string MiddleName { get; }
+        public string LastName { get; }
+        public string NameExtension { get; }
+        public string BirthDate { get; }
+        public string Deceased { get; }
+        public string GenderId { get; }
+    }
+}
