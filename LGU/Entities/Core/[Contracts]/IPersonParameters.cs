f2b738bc3b616bda183b35ac38960a778a165297
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-13 15:26:10 +0800

    Added: IPersonFields.cs; IPersonParameters.cs;

---
+++ LGU/Entities/Core/[Contracts]/IPersonParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace LGU.Entities.Core
+{
+    public interface IPersonParameters
+    {
+        string Id { get; }
+        string FirstName { get; }
+        string MiddleName { get; }
+        string LastName { get; }
+        string NameExtension { get; }
+        string BirthDate { get; }
+        string Deceased { get; }
+        string GenderId { get; }
+    }
+}
