61a5a117202fa8ead20be62675d333d80064c68a
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-15 18:28:27 +0800

    Added: IEmployeeFlexWorkScheduleManager.cs;

---
+++ LGU/EntityManagers/HumanResource/[Contracts]/IEmployeeFlexWorkScheduleManager.cs
@@ -0,0 +1,24 @@
+﻿using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityManagers.HumanResource
+{
+    public interface IEmployeeFlexWorkScheduleManager
+    {
+        IProcessResult<IEmployeeFlexWorkSchedule> Insert(IEmployeeFlexWorkSchedule employeeFlexWorkSchedule);
+        Task<IProcessResult<IEmployeeFlexWorkSchedule>> InsertAsync(IEmployeeFlexWorkSchedule employeeFlexWorkSchedule);
+        Task<IProcessResult<IEmployeeFlexWorkSchedule>> InsertAsync(IEmployeeFlexWorkSchedule employeeFlexWorkSchedule, CancellationToken cancellationToken);
+        IProcessResult<IEmployeeFlexWorkSchedule> GetByDate(DateTime date);
+        Task<IProcessResult<IEmployeeFlexWorkSchedule>> GetByDateAsync(DateTime date);
+        Task<IProcessResult<IEmployeeFlexWorkSchedule>> GetByDateAsync(DateTime date, CancellationToken cancellationToken);
+        IProcessResult<IEmployeeFlexWorkSchedule> GetById(long employeeFlexWorkScheduleId);
+        Task<IProcessResult<IEmployeeFlexWorkSchedule>> GetByIdAsync(long employeeFlexWorkScheduleId);
+        Task<IProcessResult<IEmployeeFlexWorkSchedule>> GetByIdAsync(long employeeFlexWorkScheduleId, CancellationToken cancellationToken);
+        IEnumerableProcessResult<IEmployeeFlexWorkSchedule> GetListByEmployee(IEmployee employee);
+        Task<IEnumerableProcessResult<IEmployeeFlexWorkSchedule>> GetListByEmployeeAsync(IEmployee employee);
+        Task<IEnumerableProcessResult<IEmployeeFlexWorkSchedule>> GetListByEmployeeAsync(IEmployee employee, CancellationToken cancellationToken);
+    }
+}
