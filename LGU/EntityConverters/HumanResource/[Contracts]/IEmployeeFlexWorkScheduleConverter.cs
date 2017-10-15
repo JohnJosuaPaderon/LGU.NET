e7a487ee8b3cbe34db72ebc9d7c091ae65ad296a
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-15 18:27:51 +0800

    Added: IEmployeeFlexWorkScheduleConverter.cs;

---
+++ LGU/EntityConverters/HumanResource/[Contracts]/IEmployeeFlexWorkScheduleConverter.cs
@@ -0,0 +1,20 @@
+﻿using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System;
+
+namespace LGU.EntityConverters.HumanResource
+{
+    public interface IEmployeeFlexWorkScheduleConverter : IDataConverter<IEmployeeFlexWorkSchedule>
+    {
+        IDataConverterProperty<IEmployee> PEmployee { get; }
+        IDataConverterProperty<DateTime> PEffectivityDateBegin { get; }
+        IDataConverterProperty<DateTime> PEffectivityDateEnd { get; }
+        IDataConverterProperty<IWorkTimeSchedule> PSundayWorkTimeSchedule { get; }
+        IDataConverterProperty<IWorkTimeSchedule> PMondayWorkTimeSchedule { get; }
+        IDataConverterProperty<IWorkTimeSchedule> PTuesdayWorkTimeSchedule { get; }
+        IDataConverterProperty<IWorkTimeSchedule> PWednesdayWorkTimeSchedule { get; }
+        IDataConverterProperty<IWorkTimeSchedule> PThursdayWorkTimeSchedule { get; }
+        IDataConverterProperty<IWorkTimeSchedule> PFridayWorkTimeSchedule { get; }
+        IDataConverterProperty<IWorkTimeSchedule> PSaturdayWorkTimeSchedule { get; }
+    }
+}
