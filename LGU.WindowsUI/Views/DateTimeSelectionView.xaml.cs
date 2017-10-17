9563420cc3e20af99f18b15c86b8002e9e436876
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-17 09:53:16 +0800

    Added: DateTimeSelectionView.xaml; DateTimeSelectionView/xaml.cs;

---
+++ LGU.WindowsUI/Views/DateTimeSelectionView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views
+{
+    /// <summary>
+    /// Interaction logic for DateTimeSelectionView
+    /// </summary>
+    public partial class DateTimeSelectionView : UserControl
+    {
+        public DateTimeSelectionView()
+        {
+            InitializeComponent();
+        }
+    }
+}
