370023f1345a6934ca136261f31d1004750a720a
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-17 09:52:27 +0800

    Added: AddEditCalendarEventDialogView.xaml; AddEditCalendarEventDialogView.xaml.cs;

---
+++ LGU.WindowsUI/Views/HumanResource/Dialogs/AddEditCalendarEventDialogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for AddEditCalendarEventDialogView
+    /// </summary>
+    public partial class AddEditCalendarEventDialogView : UserControl
+    {
+        public AddEditCalendarEventDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
