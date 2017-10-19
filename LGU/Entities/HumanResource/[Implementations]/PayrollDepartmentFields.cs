7b61e9800128e2e483b3fb2848e385c8fd869e38
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-19 09:25:31 +0800

    Added: PayrollDepartmentFields.cs; PayrollDepartmentParameters.cs;

---
+++ LGU/Entities/HumanResource/[Implementations]/PayrollDepartmentFields.cs
@@ -0,0 +1,18 @@
+﻿namespace LGU.Entities.HumanResource
+{
+    public class PayrollDepartmentFields : IPayrollDepartmentFields
+    {
+        public PayrollDepartmentFields()
+        {
+            DepartmentId = "DepartmentId";
+            PayrollId = "PayrollId";
+            HeadId = "HeadId";
+            Ordinal = "Ordinal";
+        }
+
+        public string DepartmentId { get; }
+        public string PayrollId { get; }
+        public string HeadId { get; }
+        public string Ordinal { get; }
+    }
+}
---
+++ LGU/Entities/HumanResource/[Implementations]/PayrollDepartmentParameters.cs
@@ -0,0 +1,18 @@
+﻿namespace LGU.Entities.HumanResource
+{
+    public class PayrollDepartmentParameters : IPayrollDepartmentParameters
+    {
+        public PayrollDepartmentParameters()
+        {
+            DepartmentId = "@_DepartmentId";
+            PayrollId = "@_PayrollId";
+            HeadId = "@_HeadId";
+            Ordinal = "@_Ordinal";
+        }
+
+        public string DepartmentId { get; }
+        public string PayrollId { get; }
+        public string HeadId { get; }
+        public string Ordinal { get; }
+    }
+}
