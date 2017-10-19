55ce1e8cefd7e3307cea6698cf3b79fd459c3141
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-19 09:26:32 +0800

    Added: EmployeeTypeToBooleanConverter.cs; EmployeeTypeToVisibilityConverter.cs;

---
+++ LGU.WindowsUI/Converters/EmployeeTypeToBooleanConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace LGU.Converters
+{
+    public sealed class EmployeeTypeToBooleanConverter : IValueConverter
+    {
+        static EmployeeTypeToBooleanConverter()
+        {
+            Instance = new EmployeeTypeToBooleanConverter();
+        }
+
+        public static EmployeeTypeToBooleanConverter Instance { get; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (short)value == System.Convert.ToInt16(parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ LGU.WindowsUI/Converters/EmployeeTypeToVisibilityConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace LGU.Converters
+{
+    public sealed class EmployeeTypeToVisibilityConverter : IValueConverter
+    {
+        static EmployeeTypeToVisibilityConverter()
+        {
+            Instance = new EmployeeTypeToVisibilityConverter();
+        }
+
+        public static EmployeeTypeToVisibilityConverter Instance { get; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (short)value == System.Convert.ToInt16(parameter) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
