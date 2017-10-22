eb59564943f30ed9e8eac73b781c100f62c29328
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-22 09:41:10 +0800

    Added: GeneratePayrollContractualDepartmentList.cs; GeneratePayrollContractualEmployeeListByDepartment.cs;

---
+++ LGU/EntityProcesses/HumanResource/[Implementations]/GeneratePayrollContractualDepartmentList.cs
@@ -0,0 +1,57 @@
+﻿using LGU.Data.Extensions;
+using LGU.Data.Rdbms;
+using LGU.Entities.HumanResource;
+using LGU.EntityConverters.HumanResource;
+using LGU.Processes;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public sealed class GeneratePayrollContractualDepartmentList : HumanResourceProcessBaseV2, IGeneratePayrollContractualDepartmentList
+    {
+        private const string PARAM_CUT_OFF_BEGIN = "@_CutOffBegin";
+        private const string PARAM_CUT_OFF_END = "@_CutOffEnd";
+
+        public GeneratePayrollContractualDepartmentList(IConnectionStringSource connectionStringSource, IGeneratePayrollContractualEmployeeListByDepartment getEmployees, IPayrollContractualDepartmentConverter converter) : base(connectionStringSource)
+        {
+            _Converter = converter;
+            _GetEmployees = getEmployees;
+
+            _Converter.GetEmployees = getEmployees;
+            _Converter.GetEmployeesInitializer = GetEmployeesInitializer;
+        }
+
+        private readonly IPayrollContractualDepartmentConverter _Converter;
+        private IGeneratePayrollContractualEmployeeListByDepartment _GetEmployees;
+
+        public ValueRange<DateTime> CutOff { get; set; }
+
+        private SqlQueryInfo QueryInfo =>
+            SqlQueryInfo.CreateProcedureQueryInfo(GetQualifiedDbObjectName())
+            .AddInputParameter(PARAM_CUT_OFF_BEGIN, CutOff.Begin)
+            .AddInputParameter(PARAM_CUT_OFF_END, CutOff.End);
+
+        private void GetEmployeesInitializer((IDepartment Department, IPayroll Payroll, IEmployee Head) arg)
+        {
+            _GetEmployees.CutOff = CutOff;
+            _GetEmployees.Department = arg.Department;
+        }
+
+        public IEnumerableProcessResult<IPayrollContractualDepartment> Execute()
+        {
+            return _SqlHelper.ExecuteReaderEnumerable(QueryInfo, _Converter);
+        }
+
+        public Task<IEnumerableProcessResult<IPayrollContractualDepartment>> ExecuteAsync()
+        {
+            return _SqlHelper.ExecuteReaderEnumerableAsync(QueryInfo, _Converter);
+        }
+
+        public Task<IEnumerableProcessResult<IPayrollContractualDepartment>> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            return _SqlHelper.ExecuteReaderEnumerableAsync(QueryInfo, _Converter, cancellationToken);
+        }
+    }
+}
---
+++ LGU/EntityProcesses/HumanResource/[Implementations]/GeneratePayrollContractualEmployeeListByDepartment.cs
@@ -0,0 +1,52 @@
+﻿using LGU.Data.Extensions;
+using LGU.Data.Rdbms;
+using LGU.Entities.HumanResource;
+using LGU.EntityConverters.HumanResource;
+using LGU.Processes;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public sealed class GeneratePayrollContractualEmployeeListByDepartment : HumanResourceProcessBaseV2, IGeneratePayrollContractualEmployeeListByDepartment
+    {
+        private const string PARAM_DEPARTMENT_ID = "@_DepartmentId";
+        private const string PARAM_CUT_OFF_BEGIN = "@_CutOffBegin";
+        private const string PARAM_CUT_OFF_END = "@_CutOffEnd";
+
+        public GeneratePayrollContractualEmployeeListByDepartment(IConnectionStringSource connectionStringSource, IPayrollContractualEmployeeConverter converter) : base(connectionStringSource)
+        {
+            _Converter = converter;
+        }
+
+        private IPayrollContractualEmployeeConverter _Converter;
+
+        public IDepartment Department { get; set; }
+        public ValueRange<DateTime> CutOff { get; set; }
+
+        private SqlQueryInfo QueryInfo =>
+            SqlQueryInfo.CreateProcedureQueryInfo(GetQualifiedDbObjectName())
+            .AddInputParameter(PARAM_DEPARTMENT_ID, Department.Id)
+            .AddInputParameter(PARAM_CUT_OFF_BEGIN, CutOff.Begin)
+            .AddInputParameter(PARAM_CUT_OFF_END, CutOff.End);
+
+        public IEnumerableProcessResult<IPayrollContractualEmployee> Execute()
+        {
+            _Converter.PDepartment.Value = null;
+            return _SqlHelper.ExecuteReaderEnumerable(QueryInfo, _Converter);
+        }
+
+        public Task<IEnumerableProcessResult<IPayrollContractualEmployee>> ExecuteAsync()
+        {
+            _Converter.PDepartment.Value = null;
+            return _SqlHelper.ExecuteReaderEnumerableAsync(QueryInfo, _Converter);
+        }
+
+        public Task<IEnumerableProcessResult<IPayrollContractualEmployee>> ExecuteAsync(CancellationToken cancellationToken)
+        {
+            _Converter.PDepartment.Value = null;
+            return _SqlHelper.ExecuteReaderEnumerableAsync(QueryInfo, _Converter, cancellationToken);
+        }
+    }
+}
