a5f6b7ab1d70e7c53cd4c969e14f314dfa118c9d
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-22 09:40:35 +0800

    Added: IGeneratePayrollContractualDepartmentList.cs; IGeneratePayrollContractualEmployeeListByDepartment.cs;

---
+++ LGU/EntityProcesses/HumanResource/[Contracts]/IGeneratePayrollContractualDepartmentList.cs
@@ -0,0 +1,11 @@
+﻿using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public interface IGeneratePayrollContractualDepartmentList : IEnumerableProcess<IPayrollContractualDepartment>
+    {
+        ValueRange<DateTime> CutOff { get; set; }
+    }
+}
---
+++ LGU/EntityProcesses/HumanResource/[Contracts]/IGeneratePayrollContractualEmployeeListByDepartment.cs
@@ -0,0 +1,12 @@
+﻿using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System;
+
+namespace LGU.EntityProcesses.HumanResource
+{
+    public interface IGeneratePayrollContractualEmployeeListByDepartment : IEnumerableProcess<IPayrollContractualEmployee>
+    {
+        IDepartment Department { get; set; }
+        ValueRange<DateTime> CutOff { get; set; }
+    }
+}
