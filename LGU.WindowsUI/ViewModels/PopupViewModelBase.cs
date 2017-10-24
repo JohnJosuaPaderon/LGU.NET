dcf5a4418b1f609c52a3b5a5f471eacdb25a70fd
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-24 11:45:31 +0800

    Updated: PopupViewModelBase.cs;

--- LGU.WindowsUI/ViewModels/PopupViewModelBase.cs
+++ LGU.WindowsUI/ViewModels/PopupViewModelBase.cs
@@ -38,9 +38,81 @@ namespace LGU.ViewModels
             get { return Notification?.Content; }
             set
             {
-                Notification.Content = value;
-                RaisePropertyChanged();
+                if (Notification != null)
+                {
+                    Notification.Content = value;
+                    RaisePropertyChanged();
+                }
             }
+        }
+
+        protected virtual void Close()
+        {
+            FinishInteraction?.Invoke();
+        }
+
+        private void ResetMouseCapture()
+        {
+            Mouse.Capture(null);
+        }
+    }
+
+    public abstract class PopupViewModelBase<TNotification> : BindableBase, IInteractionRequestAware
+        where TNotification : INotification
+    {
+        public PopupViewModelBase(IEventAggregator eventAggregator)
+        {
+            _EventAggregator = eventAggregator;
+            _NewMessageEvent = _EventAggregator.GetEvent<NewMessageEvent>();
+
+            CloseCommand = new DelegateCommand(Close);
+            ResetMouseCaptureCommand = new DelegateCommand(ResetMouseCapture);
+            InitializeCommand = new DelegateCommand(Initialize);
+        }
+
+        protected readonly IEventAggregator _EventAggregator;
+        protected readonly NewMessageEvent _NewMessageEvent;
+
+        public DelegateCommand CloseCommand { get; }
+        public DelegateCommand ResetMouseCaptureCommand { get; }
+        public DelegateCommand InitializeCommand { get; }
+        public Action FinishInteraction { get; set; }
+
+        private TNotification _Notification;
+        public INotification Notification
+        {
+            get { return _Notification; }
+            set
+            {
+                SetProperty(ref _Notification, (TNotification)value, () => Content = value.Content);
+            }
+        }
+
+        public object Content
+        {
+            get { return Notification?.Content; }
+            set
+            {
+                if (Notification != null)
+                {
+                    Notification.Content = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        protected TNotification PopupNotification
+        {
+            get { return _Notification; }
+            set
+            {
+                SetProperty(ref _Notification, value, () => Content = value.Content);
+            }
+        }
+
+        protected virtual void Initialize()
+        {
+            // TODO: Initialization
         }
 
         protected virtual void Close()
