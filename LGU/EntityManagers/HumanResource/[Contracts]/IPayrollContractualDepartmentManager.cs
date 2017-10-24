7f98d1af0ca235d3966147e813fd62fb68ebd1d7
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-24 11:40:40 +0800

    IPayrollContractualDepartmentManager.cs;

---
+++ LGU/EntityManagers/HumanResource/[Contracts]/IPayrollContractualDepartmentManager.cs
@@ -0,0 +1,15 @@
+﻿using LGU.Entities.HumanResource;
+using LGU.Processes;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LGU.EntityManagers.HumanResource
+{
+    public interface IPayrollContractualDepartmentManager
+    {
+        IEnumerableProcessResult<IPayrollContractualDepartment> GenerateList(ValueRange<DateTime> cutOff);
+        Task<IEnumerableProcessResult<IPayrollContractualDepartment>> GenerateListAsync(ValueRange<DateTime> cutOff);
+        Task<IEnumerableProcessResult<IPayrollContractualDepartment>> GenerateListAsync(ValueRange<DateTime> cutOff, CancellationToken cancellationToken);
+    }
+}
