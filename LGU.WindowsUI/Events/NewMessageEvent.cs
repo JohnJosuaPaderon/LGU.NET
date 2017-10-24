5726b31be9165099aa98119c5cb970906aa0f458
johnpad53 <johnjosua.paderon@outlook.com>
2017-10-24 11:42:28 +0800

    Updated: NewMessageEvent.cs;

--- LGU.WindowsUI/Events/NewMessageEvent.cs
+++ LGU.WindowsUI/Events/NewMessageEvent.cs
@@ -1,8 +1,23 @@
 ﻿using Prism.Events;
+using System;
 
 namespace LGU.Events
 {
     public sealed class NewMessageEvent : PubSubEvent<string>
     {
+        public void EnqueueMessage(string message)
+        {
+            Publish(message);
+        }
+
+        public void EnqueueErrorMessage(string message)
+        {
+            Publish($"Error: {message}");
+        }
+
+        public void EnqueueException(Exception exception)
+        {
+            Publish(ApplicationDomain.DebugMode ? $"Exception: {exception.Message}" : "An exception has been thrown.");
+        }
     }
 }
