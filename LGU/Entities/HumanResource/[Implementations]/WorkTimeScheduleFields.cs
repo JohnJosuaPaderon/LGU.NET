e6137b2b300693767288f6f8f11cd7a5b1bfc2b4
johnpad53 <johnjosua.paderon@outlook.com>
2017-11-01 11:00:12 +0800

    Added: WorkTimeScheduleFields.cs; WorkTimeScheduleParameters.cs;

---
+++ LGU/Entities/HumanResource/[Implementations]/WorkTimeScheduleFields.cs
@@ -0,0 +1,20 @@
+﻿namespace LGU.Entities.HumanResource
+{
+    public sealed class WorkTimeScheduleFields : EntityFields, IWorkTimeScheduleFields
+    {
+        public WorkTimeScheduleFields()
+        {
+            Description = "Description";
+            WorkTimeStart = "WorkTimeStart";
+            WorkTimeEnd = "WorkTimeEnd";
+            BreakTime = "BreakTime";
+            WorkingMonthDays = "WorkingMonthDays";
+        }
+
+        public string Description { get; }
+        public string WorkTimeStart { get; }
+        public string WorkTimeEnd { get; }
+        public string BreakTime { get; }
+        public string WorkingMonthDays { get; }
+    }
+}
---
+++ LGU/Entities/HumanResource/[Implementations]/WorkTimeScheduleParameters.cs
@@ -0,0 +1,20 @@
+﻿namespace LGU.Entities.HumanResource
+{
+    public sealed class WorkTimeScheduleParameters : EntityParameters, IWorkTimeScheduleParameters
+    {
+        public WorkTimeScheduleParameters()
+        {
+            Description = "@_Description";
+            WorkTimeStart = "@_WorkTimeStart";
+            WorkTimeEnd = "@_WorkTimeEnd";
+            BreakTime = "@_BreakTime";
+            WorkingMonthDays = "@_WorkingMonthDays";
+        }
+
+        public string Description { get; }
+        public string WorkTimeStart { get; }
+        public string WorkTimeEnd { get; }
+        public string BreakTime { get; }
+        public string WorkingMonthDays { get; }
+    }
+}
