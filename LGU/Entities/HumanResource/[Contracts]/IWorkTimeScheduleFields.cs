e02871fef3f0a2f239a56ced002d9e5710de6ba3
johnpad53 <johnjosua.paderon@outlook.com>
2017-11-01 10:59:37 +0800

    Added: IWorkTimeScheduleFields.cs; IWorkTimeScheduleParameters.cs;

---
+++ LGU/Entities/HumanResource/[Contracts]/IWorkTimeScheduleFields.cs
@@ -0,0 +1,11 @@
+﻿namespace LGU.Entities.HumanResource
+{
+    public interface IWorkTimeScheduleFields : IEntityFields
+    {
+        string Description { get; }
+        string WorkTimeStart { get; }
+        string WorkTimeEnd { get; }
+        string BreakTime { get; }
+        string WorkingMonthDays { get; }
+    }
+}
---
+++ LGU/Entities/HumanResource/[Contracts]/IWorkTimeScheduleParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace LGU.Entities.HumanResource
+{
+    public interface IWorkTimeScheduleParameters : IEntityParameters
+    {
+        string Description { get; }
+        string WorkTimeStart { get; }
+        string WorkTimeEnd { get; }
+        string BreakTime { get; }
+        string WorkingMonthDays { get; }
+    }
+}
