6d8d83c6f01c43823b702594f1c921708d256367
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-03 14:03:41 +0800

    Added: IPayrollTypeInitializer.cs;

---
+++ LGU/Entities/HumanResource/[Contracts]/IPayrollTypeInitializer.cs
@@ -0,0 +1,8 @@
+﻿namespace LGU.Entities.HumanResource
+{
+    public interface IPayrollTypeInitializer
+    {
+        IPayrollType Regular { get; }
+        IPayrollType Contractual { get; }
+    }
+}
