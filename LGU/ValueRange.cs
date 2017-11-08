e0f1b5a4265a15a9f60dae493a1606f3dc9ef310
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-08 08:11:20 +0800

    Updated: ValueRange.cs;

--- LGU/ValueRange.cs
+++ LGU/ValueRange.cs
@@ -16,5 +16,39 @@
 
         public T Begin { get; set; }
         public T End { get; set; }
+
+        public static bool operator ==(ValueRange<T> left, ValueRange<T> right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ValueRange<T> left, ValueRange<T> right)
+        {
+            return !(left == right);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (GetType() != obj.GetType()) return false;
+
+            if (obj is ValueRange<T> value)
+            {
+                return (
+                    (Equals(Begin, default(T)) || Equals(End, default(T))) ||
+                    (Equals(value.Begin, default(T)) || Equals(value.End, default(T)))) ? false :
+                    (Equals(Begin, value.Begin) && Equals(End, value.End));
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Begin.GetHashCode();
+        }
     }
 }
