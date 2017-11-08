b3822131f443eb24f7d5fb2a25a112c5968299d5
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-08 08:08:49 +0800

    Added: PayrollTypeFactory.cs;

---
+++ LGU/Entities/HumanResource/PayrollTypeFactory.cs
@@ -0,0 +1,21 @@
+﻿using LGU.EntityManagers.HumanResource;
+
+namespace LGU.Entities.HumanResource
+{
+    public static class PayrollTypeFactory
+    {
+        static PayrollTypeFactory()
+        {
+            var manager = ApplicationDomain.GetService<IPayrollTypeManager>();
+
+            var regularResult = manager.GetById(1);
+            var contractualResult = manager.GetById(2);
+
+            Regular = regularResult.Data;
+            Contractual = contractualResult.Data;
+        }
+
+        public static IPayrollType Regular { get; }
+        public static IPayrollType Contractual { get; }
+    }
+}
