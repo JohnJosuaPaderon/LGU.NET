87498f83dbbdda18e87a4bf99e515881ef7b556e
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-08 08:13:58 +0800

    Various updates on Views.

---
+++ LGU.WindowsUI/Views/HumanResource/Dialogs/ContractualPayrollSignatoryDialogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for ContractualPayrollSignatoryDialogView
+    /// </summary>
+    public partial class ContractualPayrollSignatoryDialogView : UserControl
+    {
+        public ContractualPayrollSignatoryDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
