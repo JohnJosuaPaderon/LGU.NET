d5208e33ad6aacb852354c9f72d9e87293f11e13
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-08 08:11:32 +0800

    Updated: WordExportBase.cs;

--- LGU.Windows/Reports/WordExportBase.cs
+++ LGU.Windows/Reports/WordExportBase.cs
@@ -84,7 +84,7 @@ namespace LGU.Reports
             {
                 try
                 {
-                    Document.SaveAs2(filePath);
+                    Document.SaveAs(filePath);
                     EventHandler?.OnExported(filePath);
                 }
                 catch (Exception ex)
