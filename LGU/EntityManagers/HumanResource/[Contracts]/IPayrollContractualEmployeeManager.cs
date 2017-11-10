3684aa73d5323182d9a80942afa4a53bbe5fc2b6
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-10 17:59:29 +0800

    Updated: IPayrollContractualEmployeeManager.cs; IPayrollContractualManager.cs;

--- LGU/EntityManagers/HumanResource/[Contracts]/IPayrollContractualEmployeeManager.cs
+++ LGU/EntityManagers/HumanResource/[Contracts]/IPayrollContractualEmployeeManager.cs
@@ -1,19 +1,17 @@
 ﻿using LGU.Entities.HumanResource;
 using LGU.Processes;
 using System;
-using System.Data.Common;
+using System.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace LGU.EntityManagers.HumanResource
 {
-    public interface IPayrollContractualEmployeeManager<TConnection, TTransaction>
-        where TConnection : DbConnection
-        where TTransaction : DbTransaction
+    public interface IPayrollContractualEmployeeManager
     {
-        IProcessResult<IPayrollContractualEmployee> Insert(IPayrollContractualEmployee payrollContractualEmployee, TConnection connection, TTransaction transaction);
-        Task<IProcessResult<IPayrollContractualEmployee>> InsertAsync(IPayrollContractualEmployee payrollContractualEmployee, TConnection connection, TTransaction transaction);
-        Task<IProcessResult<IPayrollContractualEmployee>> InsertAsync(IPayrollContractualEmployee payrollContractualEmployee, TConnection connection, TTransaction transaction, CancellationToken cancellationToken);
+        IProcessResult<IPayrollContractualEmployee> Insert(IPayrollContractualEmployee payrollContractualEmployee, SqlConnection connection, SqlTransaction transaction);
+        Task<IProcessResult<IPayrollContractualEmployee>> InsertAsync(IPayrollContractualEmployee payrollContractualEmployee, SqlConnection connection, SqlTransaction transaction);
+        Task<IProcessResult<IPayrollContractualEmployee>> InsertAsync(IPayrollContractualEmployee payrollContractualEmployee, SqlConnection connection, SqlTransaction transaction, CancellationToken cancellationToken);
         IEnumerableProcessResult<IPayrollContractualEmployee> GenerateList(ValueRange<DateTime> cutOff);
         Task<IEnumerableProcessResult<IPayrollContractualEmployee>> GenerateListAsync(ValueRange<DateTime> cutOff);
         Task<IEnumerableProcessResult<IPayrollContractualEmployee>> GenerateListAsync(ValueRange<DateTime> cutOff, CancellationToken cancellationToken);
--- LGU/EntityManagers/HumanResource/[Contracts]/IPayrollContractualManager.cs
+++ LGU/EntityManagers/HumanResource/[Contracts]/IPayrollContractualManager.cs
@@ -10,5 +10,8 @@ namespace LGU.EntityManagers.HumanResource
         IProcessResult<IPayrollContractual> GetById(long payrollContractualId);
         Task<IProcessResult<IPayrollContractual>> GetByIdAsync(long payrollContractualId);
         Task<IProcessResult<IPayrollContractual>> GetByIdAsync(long payrollContractualId, CancellationToken cancellationToken);
+        IProcessResult<IPayrollContractual> Insert(IPayrollContractual payrollContractual);
+        Task<IProcessResult<IPayrollContractual>> InsertAsync(IPayrollContractual payrollContractual);
+        Task<IProcessResult<IPayrollContractual>> InsertAsync(IPayrollContractual payrollContractual, CancellationToken cancellationToken);
     }
 }
