a0038c7c62bec7c5cfbd024666958479679c441a
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-10 09:42:15 +0800

    Added: BusyAppEvent.cs;

---
+++ LGU.WindowsUI/Events/BusyAppEvent.cs
@@ -0,0 +1,17 @@
+﻿using Prism.Events;
+
+namespace LGU.Events
+{
+    public sealed class BusyAppEvent : PubSubEvent<bool>
+    {
+        public void Busy()
+        {
+            Publish(true);
+        }
+
+        public void Unbusy()
+        {
+            Publish(false);
+        }
+    }
+}
