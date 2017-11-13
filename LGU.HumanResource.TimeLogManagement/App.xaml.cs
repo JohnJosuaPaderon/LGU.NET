4c6ab9ca448fdd6bdf153522b39f71a7a53819b4
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-13 09:52:22 +0800

    Added: App.config; App.xaml; App.xaml.cs; Bootsrapper.cs; LGU.HumanResource.TimeLogManagement.csproj; logo.ico; packages.config;

---
+++ LGU.HumanResource.TimeLogManagement/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using LGU.ViewModels;
+using LGU.Views.HumanResource;
+using System.Windows;
+
+namespace LGU.HumanResource.TimeLogManagement
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindowViewModel.InitialMainContentRegionSource = nameof(TimeLogManagementView);
+            new Bootstrapper().Run();
+        }
+    }
+}
---
+++ LGU.HumanResource.TimeLogManagement/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using LGU.Views.HumanResource;
+using Prism.Unity;
+
+namespace LGU.HumanResource
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            Container.RegisterTypeForNavigation<TimeLogManagementView>();
+        }
+
+        protected override void InitializeServices()
+        {
+            base.InitializeServices();
+        }
+    }
+}
