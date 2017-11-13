cc9f407ade07addb4915fdf8f3c347bbb3983fea
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-13 09:55:19 +0800

    Added: TimeLogManagementView.xaml; TimeLogManagementView.xaml.cs;

---
+++ LGU.WindowsUI/Views/HumanResource/TimeLogManagementView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource
+{
+    /// <summary>
+    /// Interaction logic for TimeLogManagementView
+    /// </summary>
+    public partial class TimeLogManagementView : UserControl
+    {
+        public TimeLogManagementView()
+        {
+            InitializeComponent();
+        }
+    }
+}
