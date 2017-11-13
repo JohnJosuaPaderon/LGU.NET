bf9c4c11bcb243ad80354e03b89c7c72fb044b6b
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-13 09:54:58 +0800

    Added: AddEditTimeLogDialogView.xaml; AddEditTimeLogDialogView.xaml.cs;

---
+++ LGU.WindowsUI/Views/HumanResource/Dialogs/AddEditTimeLogDialogView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LGU.Views.HumanResource.Dialogs
+{
+    /// <summary>
+    /// Interaction logic for AddEditTimeLogDialogView
+    /// </summary>
+    public partial class AddEditTimeLogDialogView : UserControl
+    {
+        public AddEditTimeLogDialogView()
+        {
+            InitializeComponent();
+        }
+    }
+}
