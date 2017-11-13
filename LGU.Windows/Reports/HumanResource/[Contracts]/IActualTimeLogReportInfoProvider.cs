e4ec8edd729f9e9eb1895016076a5fd9f7461db1
john josua paderon <johnjosua.paderon@outlook.com>
2017-11-13 11:04:25 +0800

    Added: IActualTimeLogReportInfoProvider.cs;

---
+++ LGU.Windows/Reports/HumanResource/[Contracts]/IActualTimeLogReportInfoProvider.cs
@@ -0,0 +1,15 @@
+﻿namespace LGU.Reports.HumanResource
+{
+    public interface IActualTimeLogReportInfoProvider
+    {
+        string Template { get; }
+        string SaveDirectory { get; }
+        bool PrintAfterSave { get; }
+        int LogRowStart { get; }
+        ExcelCell EmployeeCell { get; }
+        ExcelCell DepartmentCell { get; }
+        ExcelCell CutOffCell { get; }
+        int LoginColumn { get; }
+        int LogoutColumn { get; }
+    }
+}
