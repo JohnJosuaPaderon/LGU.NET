26ba4763eca461df1ebb482c10b50ef1bb1bf1b0
john josua paderon <johnjosua.paderon@outlook.com>
2018-01-09 18:34:23 +0800

    Updated: ActualTimeLogExportViewModel.cs;

--- LGU.WindowsUI/ViewModels/HumanResource/ActualTimeLogExportViewModel.cs
+++ LGU.WindowsUI/ViewModels/HumanResource/ActualTimeLogExportViewModel.cs
@@ -1,38 +1,46 @@
-﻿using LGU.EntityManagers.HumanResource;
+﻿using LGU.Entities.HumanResource;
+using LGU.EntityManagers.HumanResource;
 using LGU.Models;
 using LGU.Models.HumanResource;
 using LGU.Processes;
+using LGU.Reports;
+using LGU.Reports.HumanResource;
 using Prism.Commands;
 using Prism.Events;
 using Prism.Regions;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace LGU.ViewModels.HumanResource
 {
-    public sealed class ActualTimeLogExportViewModel : ViewModelBase
+    public sealed class ActualTimeLogExportViewModel : ViewModelBase, IExportEventHandler
     {
         public ActualTimeLogExportViewModel(IRegionManager regionManager, IEventAggregator eventAggregator) : base(regionManager, eventAggregator)
         {
-            r_EmployeeManager = ApplicationDomain.GetService<IEmployeeManager>();
-            r_TimeLogManager = ApplicationDomain.GetService<ITimeLogManager>();
+            _EmployeeManager = ApplicationDomain.GetService<IEmployeeManager>();
+            _TimeLogManager = ApplicationDomain.GetService<ITimeLogManager>();
+            _HumanResourceReport = ApplicationDomain.GetService<IHumanResourceReport>();
 
             Employees = new ObservableCollection<EmployeeModel>();
             TimeLogs = new ObservableCollection<TimeLogModel>();
             GetEmployeesCommand = new DelegateCommand(GetEmployees);
             GetTimeLogsCommand = new DelegateCommand(GetTimeLogs);
+            ExportCommand = new DelegateCommand(Export);
             CutOff = new ValueRangeModel<DateTime>(new ValueRange<DateTime>(DateTime.Now));
 
             _HeaderEvent.Publish("Actual Time Logs");
         }
 
-        private readonly IEmployeeManager r_EmployeeManager;
-        private readonly ITimeLogManager r_TimeLogManager;
+        private readonly IEmployeeManager _EmployeeManager;
+        private readonly ITimeLogManager _TimeLogManager;
+        private readonly IHumanResourceReport _HumanResourceReport;
 
         public DelegateCommand GetEmployeesCommand { get; }
         public DelegateCommand SearchEmployeesCommand { get; }
         public DelegateCommand GetTimeLogsCommand { get; }
+        public DelegateCommand ExportCommand { get; }
         public ObservableCollection<EmployeeModel> Employees { get; }
         public ObservableCollection<TimeLogModel> TimeLogs { get; }
 
@@ -57,11 +65,36 @@ namespace LGU.ViewModels.HumanResource
             set { SetProperty(ref _EmployeeSearchKey, value); }
         }
 
+        private async void Export()
+        {
+            if (SelectedEmployee != null && CutOff != null && TimeLogs.Any())
+            {
+                _BusyAppEvent.Busy();
+                var timeLogs = new List<ITimeLog>();
+
+                foreach (var timeLog in TimeLogs)
+                {
+                    timeLogs.Add(timeLog.GetSource());
+                }
+
+                try
+                {
+                    await _HumanResourceReport.ExportActualTimeLogAsync(SelectedEmployee.GetSource(), timeLogs, CutOff.GetSource(), this);
+                }
+                catch (Exception ex)
+                {
+                    _NewMessageEvent.EnqueueException(ex);
+                }
+
+                _BusyAppEvent.Unbusy();
+            }
+        }
+        
         private async void GetEmployees()
         {
             Employees.Clear();
             var cutOff = CutOff.GetSource();
-            var result = await (string.IsNullOrWhiteSpace(EmployeeSearchKey) ? r_EmployeeManager.GetListWithTimeLogAsync(cutOff) : r_EmployeeManager.SearchWithTimeLogAsync(EmployeeSearchKey, cutOff));
+            var result = await (string.IsNullOrWhiteSpace(EmployeeSearchKey) ? _EmployeeManager.GetListWithTimeLogAsync(cutOff) : _EmployeeManager.SearchWithTimeLogAsync(EmployeeSearchKey, cutOff));
 
             if (result.Status == ProcessResultStatus.Success)
             {
@@ -88,7 +121,7 @@ namespace LGU.ViewModels.HumanResource
         private async void GetTimeLogs()
         {
             TimeLogs.Clear();
-            var result = await r_TimeLogManager.GetActualListByEmployeeCutOffAsync(SelectedEmployee?.GetSource(), CutOff.GetSource());
+            var result = await _TimeLogManager.GetActualListByEmployeeCutOffAsync(SelectedEmployee?.GetSource(), CutOff.GetSource());
 
             if (result.Status == ProcessResultStatus.Success)
             {
@@ -104,6 +137,26 @@ namespace LGU.ViewModels.HumanResource
             {
                 EnqueueMessage(result.Message);
             }
+        }
+
+        public void OnException(Exception exception)
+        {
+            _NewMessageEvent.EnqueueException(exception);
+        }
+
+        public void OnError(string message)
+        {
+            _NewMessageEvent.EnqueueErrorMessage(message);
+        }
+
+        public void OnExported(string[] filePaths)
+        {
+            _NewMessageEvent.EnqueueMessage("Successfully exported.");
+        }
+
+        public void OnExported(string filePath)
+        {
+            _NewMessageEvent.EnqueueMessage("Successfully exported.");
         }
     }
 }
